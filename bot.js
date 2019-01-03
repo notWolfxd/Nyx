2355ef57c3098de923cfea3336a00dab4e31f793
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-01-03 18:58:48 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,23 @@
+const config = require("./config.json");
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-// Set the prefix
-let prefix = ".";
+client.on("ready", async () => {
+    console.log(`${client.user.username}  is online!`);
+    client.user.setGame("with tiddies");
+
+});
+
 client.on("message", (message) => {
-  // Exit and stop if the prefix is not there or if user is a bot
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
-  
-const args = message.content.slice(prefix.length).trim().split(/ +/g);
-const command = args.shift().toLowerCase();
-  
-  if(command === "iam") {
+
+    let prefix = config.prefix;
+    // Exit and stop if the prefix is not there or if user is a bot
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if(command === "test") {
     let [gamemode, position, team] = args;
     if (!position) return message.reply("To acquire a role do `.iam [Gamemode (Factions | Skyblock | Prison)] [Position (Leader | CoLeader)] [Team name]`.");
     if (!team) return message.reply("To acquire a role do `.iam [Gamemode (Factions | Skyblock | Prison)] [Position (Leader | CoLeader)] [Team name]`.");
@@ -18,14 +25,17 @@ const command = args.shift().toLowerCase();
     var leaderEmbed = new Discord.RichEmbed()
     .setDescription("Leader Request")
     .setColor("#f4392b")
+    .setThumbnail(message.author.avatarURL)
     .addField("Role requested by:", `${message.author} | ${message.author.id}`)
     .addField("They are looking for a role in the gametype:", `${gamemode}`)
     .addField("They are apart of:", `${team}`)
     .addField("In this team they are a:", `${position}`)
+    .setFooter("<:wolfy:530534132887846912> Nyx v1.2.5 | Made By: Wolf#9001")
   message.reply("Staff have been notified that you need the correct role. Please be patient.");
-  client.channels.get("530178004928823306").sendEmbed(leaderEmbed);
-  client.channels.get("530178004928823306").send("@everyone");
+  client.channels.get("517178851441508356").sendEmbed(leaderEmbed);
+  client.channels.get("517178851441508356").send("@everyone");
   }
+
 });
 
 client.login(process.env.BOT_TOKEN);

728121552931d25bf08446a1da8619089736786a
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-01-02 22:16:23 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+
+// Set the prefix
+let prefix = ".";
+client.on("message", (message) => {
+  // Exit and stop if the prefix is not there or if user is a bot
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  
+const args = message.content.slice(prefix.length).trim().split(/ +/g);
+const command = args.shift().toLowerCase();
+  
+  if(command === "iam") {
+    let [gamemode, position, team] = args;
+    var leaderEmbed = new Discord.RichEmbed()
+    .setDescription("Leader Request")
+    .setColor("#f4392b")
+    .addField("Role requested by:", `${message.author} | ${message.author.id}`)
+    .addField("They are looking for a role in the gametype:", `${gamemode}`)
+    .addField("They are apart of:", `${team}`)
+    .addField("In this team they are a:", `${position}`)
+  client.channels.get("530178004928823306").sendEmbed(leaderEmbed);
+  client.channels.get("530178004928823306").send("@everyone");
+  
+});
+
+client.login(process.env.BOT_TOKEN);

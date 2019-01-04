977f49019746001223c0ca01cb34b8f4be9d911d
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-01-04 08:23:18 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,9 +19,9 @@ client.on("message", (message) => {
 
   if(command === "iam") {
     let [gamemode, position, team] = args;
-    if (!position) return message.author.mention("| To acquire a role do: `.iam [Gamemode (Factions | Skyblock | Prison)] [Position (Leader | CoLeader)] [Team name]`.");
-    if (!team) return message.author.mention("| To acquire a role do: `.iam [Gamemode (Factions | Skyblock | Prison)] [Position (Leader | CoLeader)] [Team name]`.");
-    if (!gamemode) return message.author.mention("| To acquire a role do: `.iam [Gamemode (Factions | Skyblock | Prison)] [Position (Leader | CoLeader)] [Team name]`.");
+    if (!position) return message.reply("| To acquire a role do: `.iam [Gamemode (Factions | Skyblock | Prison)] [Position (Leader | CoLeader)] [Team name]`.");
+    if (!team) return message.reply("| To acquire a role do: `.iam [Gamemode (Factions | Skyblock | Prison)] [Position (Leader | CoLeader)] [Team name]`.");
+    if (!gamemode) return message.reply("| To acquire a role do: `.iam [Gamemode (Factions | Skyblock | Prison)] [Position (Leader | CoLeader)] [Team name]`.");
     var leaderEmbed = new Discord.RichEmbed()
     .setDescription("Leader Request")
     .setColor("#f4392b")

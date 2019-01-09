00e70fdd29dc0eea60254d15eac4ee6603719bea
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-01-08 19:12:12 -0500

    Update suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -15,7 +15,7 @@ module.exports.run = async (client, message, args) => {
     .addField("Their Suggestion Is:", `${suggestion}`)
     .setFooter("Nyx v1.2.9 | Made By: Wolf#9001", client.user.avatarURL)
   message.reply("thank you for your suggestion, it has been sent to the suggestion area for players to vote on!");
-  let voting = await client.channels.get("517178851441508356").sendEmbed(suggestEmbed);
+  let voting = await client.channels.get("512077901085278228").sendEmbed(suggestEmbed);
   await voting.react("✅");
   await voting.react("❌");
   }

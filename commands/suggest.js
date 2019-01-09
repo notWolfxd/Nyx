bcb3d136eb9a60afc0914cc0bfd2e58a5c536582
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-01-09 17:57:03 -0500

    Update suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -2,9 +2,22 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
+    //Build .suggest help Embed
+    var suggestHelpEmbed = new Discord.RichEmbed()
+    .setDescription("**Command: Suggest**")
+    .setColor("#f74e00")
+    .setThumbnail(message.author.avatarURL)
+    .addField("Description: ", "Sends a suggestion to the suggestion channel. Once sent the players/ users can vote upon what was suggested.")
+    .addField("Usage: ", ".suggest (Gamemode [Factions | Skyblock | Prison]) (Suggestion)")
+    .addField("Example: ", ".suggest Factions ╠═══╣ ets build a ladder to demote Milk ╠═══╣")
+    .setFooter("Nyx v1.3.0 | Made By: Wolf#9001", client.user.avatarURL)
+
+    //Define the arguments for .suggest.
     let gametype = args[0];
     let suggestion = args.slice(1).join(" ")
-    if (!gametype || !suggestion) return message.reply("to submit a suggestion do: `.suggest (Gamemode [Factions | Skyblock | Prison]) (Suggestion)`.");
+    if (args[0] != "factions" && args[0] != "prison" && args[0] != "skyblock") return message.channel.sendEmbed(suggestHelpEmbed);
+
+    //Build the .suggest message Embed.
     var suggestEmbed = new Discord.RichEmbed()
     .setDescription("**Suggestion**")
     .setColor("#f74e00")
@@ -13,6 +26,8 @@ module.exports.run = async (client, message, args) => {
     .addField("Suggestion For:", `${gametype}`)
     .addField("Their Suggestion Is:", `${suggestion}`)
     .setFooter("Nyx v1.2.9 | Made By: Wolf#9001", client.user.avatarURL)
+    
+  //Sending the completed message.
   message.reply("thank you for your suggestion, it has been sent to the suggestion area for players to vote on!");
   let voting = await client.channels.get("512077901085278228").sendEmbed(suggestEmbed);
   await voting.react("✅");

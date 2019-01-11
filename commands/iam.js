cd35f4cac23351164f43cea812277f2d362078b0
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-01-10 20:16:35 -0500

    Update iam.js

--- commands/iam.js
+++ commands/iam.js
@@ -4,13 +4,13 @@ module.exports.run = async (client, message, args) => {
     
     //Build .iam help Embed.
     var iamHelpEmbed = new Discord.RichEmbed()
-    .setDescription("**Command: IAm**")
+    .setTitle("Command: IAm")
     .setColor("#f4392b")
     .setThumbnail(message.author.avatarURL)
     .addField("Description: ", "Alerts the proper staff that you need a leader role in the gamemode of choice.")
     .addField("Usage: ", ".iam [Gamemode (Factions | Skyblock | Prison)] [Position (Leader | CoLeader)] [Team name]")
     .addField("Example: ", ".iam Factions Leader Promised")
-    .setFooter("Nyx v1.3.0 | Made By: Wolf#9001", client.user.avatarURL)
+    .setFooter("Nyx v1.3.1 | Made By: Wolf#9001", client.user.avatarURL)
 
     //Define the arguments for .iam.
     let [gamemode, position, team] = args;
@@ -20,14 +20,14 @@ module.exports.run = async (client, message, args) => {
  
     //Build .iam message Embed.
     var leaderEmbed = new Discord.RichEmbed()
-    .setDescription("Leader Request")
+    .setTitle("Leader Request")
     .setColor("#f4392b")
     .setThumbnail(message.author.avatarURL)
     .addField("Role requested by:", `${message.author} | ${message.author.id}`)
     .addField("They are looking for a role in the gametype:", `${gamemode}`)
     .addField("They are apart of:", `${team}`)
     .addField("In this team they are a:", `${position}`)
-    .setFooter("Nyx v1.2.5 | Made By: Wolf#9001", client.user.avatarURL)
+    .setFooter("Nyx v1.3.1 | Made By: Wolf#9001", client.user.avatarURL)
     
   //Sending the completed message.
   message.reply("staff have been notified that you need the correct role. Please be patient.");

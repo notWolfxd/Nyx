505b5d56d4059c7ec8a802acd2c7ad07bfafeff7
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-01-12 12:42:07 -0500

    Create mark.js

---
+++ commands/mark.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+message.channel.send("<@391743942070370304> is gay")
+}
+
+module.exports.help = {
+    name: "mark",
+}

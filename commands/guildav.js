e6cc385987ceb15d93292faeab25f0318fa38272
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-01-21 21:05:33 -0500

    Update and rename guildav to guildav.js

--- commands/guildav.js
+++ commands/guildav.js
@@ -2,8 +2,12 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
+let guildID = arg[0];
+
+message.channel.send(message.guild.iconURL);
+
 }
 
 module.exports.help = {
-name = "guildav"
+name: "guildav"
 }

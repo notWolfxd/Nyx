f4d386fd66b66c23853165e0cb09b306dc7598d2
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-01-29 10:11:45 -0500

    Create gay.js

---
+++ commands/gay.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+message.channel.send("<@`${message.author.id}`> is super gay.");
+}
+
+module.exports.help = {
+    name: "gay"
+}

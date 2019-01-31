27ae8b5bf63f9d92afe551bc3de019d15199efcd
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-01-31 14:56:21 -0500

    New module system for all commands.

--- bot.js
+++ bot.js
@@ -1,53 +1,9 @@
-const config = require("./config.json");
-const Discord = require("discord.js");
-const fs = require("fs");
-const client = new Discord.Client();
-client.commands = new Discord.Collection();
-
-fs.readdir("./commands/", (err, files) => {
-
-  if(err) console.log(err);
-
-  let jsfile = files.filter(f => f.split(".").pop() === "js")
-  if(jsfile.length <= 0){
-    console.log("Couldn't find commands.");
-    return;
-  }
-
-  jsfile.forEach((f, i) =>{
-    let props = require(`./commands/${f}`);
-    console.log(`${f} loaded!`);
-    client.commands.set(props.help.name, props);
-  });
+const Discord = require('discord.js');
+const client = new Discord.Client({
+fetchAllMembers: true
 });
-client.on("ready", async () => {
-  console.log(`${client.user.username} is online on ${client.guilds.size} servers!`);
+const config = require('./config.json');
+require("./handler/commandhandler")(client);
+require("./handler/eventhandler")(client);
 
-  client.user.setActivity("FREE NADEKO 2K19", {type: "PLAYING"});
-});
-
-client.on("message", async message => {
- // let prefix = config.prefix
- // if (!message.content.startsWith(prefix)) return; 
-  if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
-
-  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
-
-  if (!prefixes[message.guild.id]){
-    prefixes[message.guild.id] = {
-        prefix: config.prefix
-    };
-  }
-
-  let prefix = prefixes[message.guild.id].prefix;
-  if (!message.content.startsWith(prefix)) return; 
-
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-
-  let commandfile = client.commands.get(command.slice());
-  if(commandfile) commandfile.run(client, message, args);
-});
-
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN).catch(err => console.log(err));

f92be67abfb8fa80d872fd53a8dba39d6f65f0be
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-01-31 16:09:24 -0500

    re-adding ready shit for now

--- bot.js
+++ bot.js
@@ -6,4 +6,10 @@ const config = require('./config.json');
 require("./handler/commandhandler")(client);
 require("./handler/eventhandler")(client);
 
+    client.on("ready", async () => {
+        console.log(`${client.user.username} is online on ${client.guilds.size} servers!`);
+      
+        client.user.setActivity("FREE NADEKO 2K19", {type: "PLAYING"});
+      });
+
 client.login(process.env.BOT_TOKEN).catch(err => console.log(err));

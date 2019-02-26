f19ecd0c742812c1f487ba7bedc13f3a135c4ba8
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-02-25 22:47:10 -0500

    Create mentionrole.js

---
+++ commands/util/mentionrole.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+if (!message.member.hasPermission("MANAGE_ROLES"))
+   return;
+
+if (!args.role)
+    return message.channel.send("Specify a role!");
+
+  args.role = args.role.join(' ');
+
+  let role;
+  if (message.mentions.roles.size) {
+    role = message.mentions.roles.first();
+  }
+  else if (message.guild.roles.has(args.role)) {
+    role = message.guild.roles.get(args.role);
+  }
+  else {
+    role = message.guild.roles.find(role => role.name === args.role);
+  }
+
+  if (role.editable) {
+    await role.setMentionable(true, '`${message.author}`, with command mentionrole (Start).');
+    await message.channel.send(`<@&${role.id}>`);
+    await role.setMentionable(false, '`${message.author}`, with command mentionrole (Stop).');
+  }
+};
+
+};
+module.exports.help = {
+    name: "mentionrole",
+    aliases: [
+            "mr",
+            "menro"
+    ]
+  }

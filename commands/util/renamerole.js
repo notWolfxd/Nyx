6eda937b585c273afaaa250251f95189f4ef7367
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-02-27 17:02:36 -0500

    this.this

---
+++ commands/util/renamerole.js
@@ -0,0 +1,52 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+if (!message.member.hasPermission("MANAGE_ROLES"))
+   return;
+
+let namechange = args.join(" ").split(" | ");
+let oldname = namechange[0];
+
+if (oldname.position >= message.member.highestRole.position)
+        return message.channel.send("You do not have permission to edit this role!");
+
+let namechanging;
+        if (message.mentions.roles.size) {
+          namechanging = message.mentions.roles.first();
+        }
+        else if (message.guild.roles.has(oldname)) {
+          namechanging = message.guild.roles.get(oldname);
+        }
+        else {
+          namechanging = message.guild.roles.find(role => role.name === oldname);
+        }
+let newname = namechange[1];
+
+if (newname.length >= 100)
+        return message.channel.send("The new name you entered is too long.")
+if (!namechanging.editable)
+        return message.channel.send("I do not have sufficient enough permissions to change the name of that role.");
+if (!oldname)
+        return message.channel.send("Could not find the original role.");
+if (!newname)
+        return message.channel.send("You need to specify a new name for the role.")
+
+namechanging.setName(newname, `${message.author.username} | With command renamerole.`);
+
+var ncembed = new Discord.RichEmbed()
+   .setTitle("Role Name Changed")
+   .setColor("#f439c3")
+   .addField("Old Name", oldname, true)
+   .addField("New Name", newname,true)
+   
+message.channel.send(ncembed);
+
+};
+module.exports.help = {
+    name: "renamerole",
+    aliases: [
+            "rename",
+            "rrole"
+    ]
+  }

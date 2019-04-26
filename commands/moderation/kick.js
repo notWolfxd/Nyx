b1533917629aa25c59ca6c74308df4424193d193
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-04-26 12:19:08 -0400

    Update and rename k.js to kick.js

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -16,6 +16,9 @@ if (!args[0])
 if (member.id === message.author.id)
         return message.channel.send("You cannot kick yourself.")
 
+if (member.id === "298812170093723649")
+        return message.channel.send("You cannot ban Wolf.")
+
 if (!member.kickable)
         return message.channel.send("Something has stopped me from banning them (higher role maybe?).");
 
@@ -25,30 +28,34 @@ if (member.highestRole.position === message.member.highestRole.position)
 if (member.highestRole.position > message.member.highestRole.position)
         return message.channel.send("You cannot kick your superiors.")
 
+    member.send(`You have been kicked from **__${message.guild.name}__** by **__${message.author.username}__** for **__${reason}__**.`)
     member.kick(`${message.author.username} | For: ${reason}`).then(() => {
-        var kEmbed = new Discord.RichEmbed()
+        
+var kEmbed = new Discord.RichEmbed()
         .setTitle("⚠️ User Kicked")
         .setColor("#32CD32")
         .addField("Username", `${member.user}`, true)
         .addField("ID", `${member.user.id}`, true)
-            message.channel.send(kEmbed);
-        const embed = new Discord.RichEmbed()
-            .setTitle('User Kicked')
-            .setColor("#8B0000")
-            .setThumbnail(message.author.avatarURL)
-			.addField(`User:`, `${member.user.tag} | ${member.user.id}`)
-			.addField(`Issued By:`, `${message.author.tag} | ${message.author.id}`)
-			.addField(`Reason:`, `${reason}`)
-			.setFooter("Nyx v1.4.0 | Made By: Wolf#9001", client.user.avatarURL)
-		if (modlog) modlog.send({ embed })
-			.then(() => {
-				client.log('log', `${message.guild.name}/#${message.channel.name} (${message.channel.id}): ${member.user.tag} (${member.user.id}) was kicked by ${message.author.tag} (${message.author.id})`, 'CMD');
-			})
-			.catch((err) => {
-				console.log(err);
-			});
+message.channel.send(kEmbed);
+        
+var embed = new Discord.RichEmbed()
+        .setTitle('User Kicked')
+        .setColor("#8B0000")
+        .setThumbnail(message.author.avatarURL)
+	.addField(`User:`, `${member.user.tag} | ${member.user.id}`)
+	.addField(`Issued By:`, `${message.author.tag} | ${message.author.id}`)
+	.addField(`Reason:`, `${reason}`)
+	.setFooter("Nyx v1.4.0 | Made By: Wolf#9001", client.user.avatarURL)
+if (modlog) modlog.send({ embed })
+
+	.then(() => {
+		client.log('log', `${message.guild.name}/#${message.channel.name} (${message.channel.id}): ${member.user.tag} (${member.user.id}) was kicked by ${message.author.tag} (${message.author.id})`, 'CMD');
+		})
+	.catch((err) => {
+		console.log(err);
+		});
 	})
-		.catch(error => message.channel.send(`Unable to kick ${member} because of : ${error}`));
+	.catch(error => message.channel.send(`Unable to kick ${member} because of : ${error}`));
 
 };
 module.exports.help = {

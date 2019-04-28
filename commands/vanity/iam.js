55de66efa69cbd9f4ab9c3eae1db5b800f196797
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-04-28 12:15:47 -0400

    Update iam.js

--- commands/vanity/iam.js
+++ commands/vanity/iam.js
@@ -31,17 +31,17 @@ module.exports.run = async (client, message, args) => {
     .setFooter("Nyx v1.4.0 | Made By: Wolf#9001", client.user.avatarURL)
     
   //Sending the completed message.
-    if(args[0].toLowerCase() == "factions" && !member.roles.has(556189435796914204))
+    if(args[0].toLowerCase() == "factions" || !member.roles.has(556189435796914204))
         return member.addRole(facsrole);
-    else(args[0].toLowerCase() == "factions" && member.roles.has(556189435796914204))
+    if(args[0].toLowerCase() == "factions" || member.roles.has(556189435796914204))
         return member.removeRole(facsrole);
     if(args[0].toLowerCase() == "skyblock" && !member.roles.has(556189434672971777))
         return member.addRole(sbrole);
-    else(args[0].toLowerCase() == "skyblock" && member.roles.has(556189434672971777))
+    if(args[0].toLowerCase() == "skyblock" && member.roles.has(556189434672971777))
         return member.removeRole(sbrole);
     if(args[0].toLowerCase() == "prison" && !member.roles.has(556189416172027910))
         return member.addRole(prisonrole);
-    else(args[0].toLowerCase() == "prison" && member.roles.has(556189416172027910))
+    if(args[0].toLowerCase() == "prison" && member.roles.has(556189416172027910))
         return member.removeRole(prisonrole);
     //message.reply("staff have been notified that you need the correct role. Please be patient.");
  // client.channels.get("530178004928823306").sendEmbed(leaderEmbed);

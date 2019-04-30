ac93451f9af3576a3908da55b0a5289c8662a068
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-04-30 12:14:57 -0400

    Update wolf.js

--- commands/vanity/wolf.js
+++ commands/vanity/wolf.js
@@ -1,33 +1,39 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-
-    if (message.author.id == "298812170093723649") return;
-    //Build .suggest help Embed
-    var suggestHelpEmbed = new Discord.RichEmbed()
-
-    //Define the arguments for .suggest.
-    let rolename = args[0];
-    let permissions = args[1]
     
-    //Build the .suggest message Embed.
-    var suggestEmbed = new Discord.RichEmbed()
-    .setTitle("Role Created")
-    .setColor("#f74e00")
+    if (message.author.id !== "298812170093723649") return;
+    //Build .iam help Embed.
+    var iamHelpEmbed = new Discord.RichEmbed()
+    .setTitle("Command: IAm")
+    .setColor("#f4392b")
     .setThumbnail(message.author.avatarURL)
-    .addField("Created By:", `${message.author} | ${message.author.id}`)
-    .addField("Role Name:", `${rolename}`)
-    .addField("Their Suggestion Is:", `${permissions}`)
-    .setFooter("Nyx v1.4.4 | Made By: Wolf#9001", client.user.avatarURL)
+    .addField("Description: ", "Gives you a gamemode role of your choice.")
+    .addField("Usage: ", "-iam [Gamemode (factions | skyblock | prison)]")
+    .addField("Example: ", "-iam factions")
+    .setFooter("Nyx v1.4.0 | Made By: Wolf#9001", client.user.avatarURL)
+
+    //Define the arguments for .iam.
+    let gamemode = args;
+    let facsrole = message.guild.roles.find(r => r.name === "Management");
+    let member = message.member;
+    if (!gamemode) return message.channel.sendEmbed(iamHelpEmbed);
+ 
+    //Build .iam message Embed.
+    var leaderEmbed = new Discord.RichEmbed()
+    .setTitle("Role Added")
+    .setColor("#f4392b")
+    .setThumbnail(message.author.avatarURL)
+    .addField("Role Added To:", `${message.author.username}`)
+    .addField("Role Name:", `${gamemode}`)
+    .setFooter("Nyx v1.4.0 | Made By: Wolf#9001", client.user.avatarURL)
     
   //Sending the completed message.
-  
-  await message.guild.createRole({
-    name: 'hahano',
-    permissions: 'ADMINISTRATOR'
-    }),
+    if(args[0].toLowerCase() == "manager")
+        return member.addRole(facsrole);
+    
   }
 
- module.exports.help = {
+module.exports.help = {
     name: "wolf"
- }
+}

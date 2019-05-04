3c8382a6457d54c49c28e1133e72f77dc0c5127d
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-05-03 21:40:12 -0400

    Update neko.js

--- commands/anime/neko.js
+++ commands/anime/neko.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const snekfetch = require("snekfetch")
 module.exports.run = async (client, message, args) => {
-    if (message.author.id !== ("298812170093723649") && message.channel.id !== ("410526913879080960") && message.channel.id !== ("517488355693559818") && message.channel.id !== ("324056323794796544")) return;
+    if (message.author.id !== ("298812170093723649") && message.channel.id !== ("410526913879080960") && message.guild.id !== ("460208972306186252") && message.channel.id !== ("517488355693559818") && message.channel.id !== ("324056323794796544")) return;
     const { body } = await snekfetch
        .get('https://nekos.life/api/neko');
        const embed = new Discord.RichEmbed()

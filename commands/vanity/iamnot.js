b0724d4c8d1b4c054f121d37e4e2b43b91419a6c
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-05-06 21:22:18 -0400

    Update iamnot.js

--- commands/vanity/iamnot.js
+++ commands/vanity/iamnot.js
@@ -31,14 +31,18 @@ module.exports.run = async (client, message, args) => {
     .setFooter("Nyx v1.4.0 | Made By: Wolf#9001", client.user.avatarURL)
     
   //Sending the completed message.
-    if(args[0].toLowerCase() == "factions" && member.roles.has(556189435796914204))
-        return member.removeRole(facsrole);
-    if(args[0].toLowerCase() == "skyblock" && member.roles.has(556189434672971777))
-        return member.removeRole(sbrole);
-    if(args[0].toLowerCase() == "prison" && member.roles.has(556189416172027910))
-        return member.removeRole(prisonrole);
-    //message.reply("staff have been notified that you need the correct role. Please be patient.");
- // client.channels.get("530178004928823306").sendEmbed(leaderEmbed);
+    if(args[0].toLowerCase() == "factions" && member.roles.has(556189435796914204)) {
+        await member.removeRole(facsrole);
+        await message.channel.send(leaderEmbed);
+        }
+    if(args[0].toLowerCase() == "skyblock" && member.roles.has(556189434672971777)) {
+        await member.removeRole(sbrole);
+        await message.channel.send(leaderEmbed);
+        }
+    if(args[0].toLowerCase() == "prison" && member.roles.has(556189416172027910)) {
+        await member.removeRole(prisonrole);
+        await message.channel.send(leaderEmbed);
+        }
   }
 
 module.exports.help = {

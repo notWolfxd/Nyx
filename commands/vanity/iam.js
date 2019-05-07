18088c01278acd0543e641ed241c63692a49aee2
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-05-06 21:15:14 -0400

    Update iam.js

--- commands/vanity/iam.js
+++ commands/vanity/iam.js
@@ -31,8 +31,10 @@ module.exports.run = async (client, message, args) => {
     .setFooter("Nyx v1.4.0 | Made By: Wolf#9001", client.user.avatarURL)
     
   //Sending the completed message.
-    if(args[0].toLowerCase() == "factions" && !member.roles.has(556189435796914204))
-        return member.addRole(facsrole);
+    if(args[0].toLowerCase() == "factions" && !member.roles.has(556189435796914204)) {
+        await member.addRole(facsrole);
+        await message.channel.send(leaderEmbed);
+        },
     if(args[0].toLowerCase() == "skyblock" && !member.roles.has(556189434672971777))
         return member.addRole(sbrole);
     if(args[0].toLowerCase() == "prison" && !member.roles.has(556189416172027910))

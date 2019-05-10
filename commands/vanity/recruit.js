76bb5fb17a78db1b47a85e326696f70ad68e3522
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-05-10 15:53:09 -0400

    Update recruit.js

--- commands/vanity/recruit.js
+++ commands/vanity/recruit.js
@@ -2,10 +2,11 @@ const Discord = require("discord.js");
 const cooldown = new Set();
 
 module.exports.run = async (client, message, args) => {
-    if (!message.member.roles.has("402751364846845953") && message.channel.id !== ("410526913879080960")) 
-      return;
 
-    if (cooldown.has(message.author.id)) 
+   if (message.channel.id !== ("410526913879080960")) 
+        return;
+
+   if (cooldown.has(message.author.id)) 
       return message.reply("you must wait 5 hours between recruiting!");
     
       else {
@@ -20,7 +21,7 @@ module.exports.run = async (client, message, args) => {
     .addField("Permissions:", "You must have one of the three leader roles (faction-leader, island-leader, or gang-leader).")
     .addField("Syntax Formatter: ", "Use https://notwolfxd.github.io/leaders/ to format the recruitment post if you're having trouble.")
     .addField("Friendly PSA:", "If you are caught using this to advertise other servers, then you will be banned.")
-    .setFooter("Nyx v1.4.0 | Made By: Wolf#9001", client.user.avatarURL)
+    .setFooter("Nyx v1.4.6 | Made By: Wolf#9001", client.user.avatarURL)
 
     //Define the arguments for recruit.
     let recruitDefine = args.join(" ").split(" | ");
@@ -28,52 +29,65 @@ module.exports.run = async (client, message, args) => {
        if (!recruitDefine[0]) 
         return message.channel.sendEmbed(recruitHelpEmbed);
     let teamname = recruitDefine[1];
+
     let about = recruitDefine[2];
     about = about.replace(/(-)\s/gi, '\n$&');
+
     let req = recruitDefine[3];
     req = req.replace(/(-)\s/gi, '\n$&');  
+
     let link = recruitDefine[4];
+
     let facsaliases = [ "facs", "factions", "faction", "demonic", "heroic", "apocalyptic" ];
     let sbaliases = [ "sb", "skyblock", "island", "space", "jungle", "wildwest", "wild west" ];
     let prisonaliases = [ "prison", "prisons", "cell", "gang", "galactic", "alcatraz", "mythic" ];
 
-   // if (recruitDefine[0] !== facsaliases.toLowerCase() && recruitDefine[0] !== sbaliases.toLowerCase() && recruitDefine[0] !== prisonaliases.toLowerCase()) 
-//      return message.channel.sendEmbed(recruitHelpEmbed);
-
-    //Build the .recruit message Embed.
+    //Build the Recruitment message Embed.
     var recruitEmbed = new Discord.RichEmbed()
 
     .setTitle("Team Recruitment!")
     .setColor("#ffa0bb")
     .setThumbnail(message.author.avatarURL)
     .addField("Recruitment Format:", "https://notwolfxd.github.io/leaders")
-    .addField("Recruiter:", `${message.author.username} | ${message.author.id}`)
-    .addField("Recruiting In:", `${gametype}`)
+    .addField("Recruiter:", `${message.author.username}#${message.author.discriminator} | ${message.author.id}`)
+    .addField("Recruiting On:", `${gametype}`)
     .addField("Recruiting For:", `${teamname}`)
     .addField("About Them:", `${about} `)
     .addField("Their Requirements:", `${req}`)
     .addField("Team Discord:", `${link}`)
-    .setFooter("Nyx v1.4.0 | Made By: Wolf#9001", client.user.avatarURL)
-    
-//  Command cooldown set up.
-   cooldown.add(message.author.id);
-   setTimeout(() => {
-       cooldown.delete(message.author.id);
-   }, 18000000);
+    .setFooter("Nyx v1.4.6 | Made By: Wolf#9001", client.user.avatarURL)
 
-   if (message.member.roles.has("471779470525595668") && recruitDefine[0].toLowerCase() == "factions") 
-      return client.channels.get("426903359392448522").send(recruitEmbed);
-   if (message.member.roles.has("487054093655998485") && recruitDefine[0].toLowerCase() == "skyblock") 
-      return client.channels.get("487053977821904909").send(recruitEmbed);
-   if (message.member.roles.has("515587892525924352") && recruitDefine[0].toLowerCase() == "prison") 
-      return client.channels.get("515639377695866881").send(recruitEmbed);
-   await message.reply("you need a leader role to use this command!");
-    }
- }
+   if (message.member.roles.has("471779470525595668") && facsaliases.includes(recruitDefine[0].toLowerCase())) {
+      await client.channels.get("426903359392448522").send(recruitEmbed);
+      await message.channel.send("Your recruitment information has been sent to the respective channel. You must wait **__five__** hours to post it again!");
+      await    cooldown.add(message.author.id);
+                  setTimeout(() => {
+               cooldown.delete(message.author.id);
+               }, 18000000);
+   }
+   if (message.member.roles.has("487054093655998485") && sbaliases.includes(recruitDefine[0].toLowerCase())) {
+      await client.channels.get("487053977821904909").send(recruitEmbed);
+      await message.channel.send("Your recruitment information has been sent to the respective channel. You must wait **__five__** hours to post it again!");
+      await    cooldown.add(message.author.id);
+                  setTimeout(() => {
+               cooldown.delete(message.author.id);
+               }, 18000000);
+   }
+   if (message.member.roles.has("515587892525924352") && prisonaliases.includes(recruitDefine[0].toLowerCase())) {
+      await client.channels.get("515639377695866881").send(recruitEmbed);
+      await message.channel.send("Your recruitment information has been sent to the respective channel. You must wait **__five__** hours to post it again!");
+      await    cooldown.add(message.author.id);
+                  setTimeout(() => {
+               cooldown.delete(message.author.id);
+               }, 18000000);
+            }
+         }
+      }
  module.exports.help = {
     name: "recruit",
     aliases: [ "rec",
                "recruitment",
-               "recruiting"
+               "recruiting",
+               "r"
              ]
 }

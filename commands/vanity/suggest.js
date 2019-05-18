ce7f977a66432e36a2601c3eca85e035acc690ce
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-05-17 21:15:48 -0400

    Update suggest.js

--- commands/vanity/suggest.js
+++ commands/vanity/suggest.js
@@ -1,9 +1,8 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-
     
-    //Build .suggest help Embed
+    //Build Suggest help Embed
     var suggestHelpEmbed = new Discord.RichEmbed()
     .setTitle("Command: Suggest")
     .setColor("#f74e00")
@@ -13,28 +12,38 @@ module.exports.run = async (client, message, args) => {
     .addField("Example: ", "-suggest Factions ╠═══╣ Lets build a ladder to demote Milk ╠═══╣")
     .setFooter("Nyx v1.4.6 | Made By: Wolf#9001", client.user.avatarURL)
 
-    //Define the arguments for .suggest.
+    //Define the arguments for Suggest.
     let gametype = args[0];
-    let suggestion = args.slice(1).join(" ")
-    if (args[0] != "factions" && args[0] != "prison" && args[0] != "skyblock") return message.channel.sendEmbed(suggestHelpEmbed);
+    let suggestion = args.slice(1).join(" ");
+    let facsaliases = [ "facs", "factions", "faction", "demonic", "heroic", "apocalyptic" ];
+    let sbaliases = [ "sb", "skyblock", "island", "space", "jungle", "wildwest", "wild west" ];
+    let prisonaliases = [ "prison", "prisons", "cell", "gang", "galactic", "alcatraz", "mythic" ];
 
+    
     //Build the .suggest message Embed.
     var suggestEmbed = new Discord.RichEmbed()
     .setTitle("Suggestion")
     .setColor("#f74e00")
     .setThumbnail(message.author.avatarURL)
-    .addField("Suggested By:", `${message.author} | ${message.author.id}`)
+    .addField("Suggested By:", `${message.author}#${message.author.discriminator} | ${message.author.id}`)
     .addField("Suggestion For:", `${gametype}`)
     .addField("Their Suggestion Is:", `${suggestion}`)
     .setFooter("Nyx v1.4.6 | Made By: Wolf#9001", client.user.avatarURL)
     
   //Sending the completed message.
-  message.channel.send("Thank you for your suggestion, it has been sent to the suggestion area for players to vote on!");
-  let voting = await client.channels.get("576825389305954324").sendEmbed(suggestEmbed);
-  await voting.react("✅");
-  await voting.react("❌");
+  if (message.guild.id == ("576567811552313361") && sbaliases.includes(gamemode.toLowerCase())) {
+    let voting = await client.channels.get("576825389305954324").sendEmbed(suggestEmbed);
+    await voting.react("✅");
+    await voting.react("❌");
+    await message.channel.send("Thank you for your suggestion, it has been sent to the suggestion area for players to vote on!");
+   }
+  else {
+      await message.channel.send("You did not specify a proper Skyblock Realm alias in your gamemode area.
   }
 
  module.exports.help = {
-    name: "suggest"
+    name: "suggest",
+    aliases: [ "s",
+               "suggestion"
+             ]
  }

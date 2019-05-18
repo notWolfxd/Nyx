8be727c417956885c598dd770560b4cabc0a24ff
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-05-17 20:54:35 -0400

    Update suggest.js

--- commands/vanity/suggest.js
+++ commands/vanity/suggest.js
@@ -2,16 +2,16 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
-    if (message.guild.id == "290249616484597771") return;
+    if (message.guild.id == "576567811552313361") return;
     //Build .suggest help Embed
     var suggestHelpEmbed = new Discord.RichEmbed()
     .setTitle("Command: Suggest")
     .setColor("#f74e00")
     .setThumbnail(message.author.avatarURL)
     .addField("Description: ", "Sends a suggestion to the suggestion channel. Once sent the players/ users can vote upon what was suggested.")
-    .addField("Usage: ", "-suggest (Gamemode [factions | skyblock | prison]) (Suggestion)")
+    .addField("Usage: ", "-suggest <Gamemode [factions | skyblock | prison]> <Suggestion>")
     .addField("Example: ", "-suggest Factions ╠═══╣ Lets build a ladder to demote Milk ╠═══╣")
-    .setFooter("Nyx v1.4.0 | Made By: Wolf#9001", client.user.avatarURL)
+    .setFooter("Nyx v1.4.6 | Made By: Wolf#9001", client.user.avatarURL)
 
     //Define the arguments for .suggest.
     let gametype = args[0];
@@ -26,11 +26,11 @@ module.exports.run = async (client, message, args) => {
     .addField("Suggested By:", `${message.author} | ${message.author.id}`)
     .addField("Suggestion For:", `${gametype}`)
     .addField("Their Suggestion Is:", `${suggestion}`)
-    .setFooter("Nyx v1.4.0 | Made By: Wolf#9001", client.user.avatarURL)
+    .setFooter("Nyx v1.4.6 | Made By: Wolf#9001", client.user.avatarURL)
     
   //Sending the completed message.
-  message.reply("thank you for your suggestion, it has been sent to the suggestion area for players to vote on!");
-  let voting = await client.channels.get("512077901085278228").sendEmbed(suggestEmbed);
+  message.channel.send("Thank you for your suggestion, it has been sent to the suggestion area for players to vote on!");
+  let voting = await client.channels.get("576825389305954324").sendEmbed(suggestEmbed);
   await voting.react("✅");
   await voting.react("❌");
   }

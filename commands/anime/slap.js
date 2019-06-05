02d6731713ed6970cefe5b467effa226b82a21ed
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-06-05 14:00:29 -0400

    Create slap.js

---
+++ commands/anime/slap.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const snekfetch = require("snekfetch")
+module.exports.run = async (client, message, args) => {
+
+    if (message.author.id !== ("298812170093723649") && message.author.id !== ("216416135929790464") && message.channel.id !== ("410526913879080960") && message.channel.id !== ("324056323794796544")) return;
+      if (message.mentions.users.size < 1) return message.channel.send("You missed your target I assume.")
+      let user = message.guild.member(message.mentions.users.first());
+        snekfetch.get('https://nekos.life/api/v2/img/slap')
+            .set('Key', 'dnZ4fFJbjtch56pNbfrZeSRfgWqdPDgf')
+            .then(r => message.channel.send(`${user}, you were slapped by ${message.author.username}!`,{
+                embed: {
+                    image: {
+                        url: r.body.url
+                    }
+                }
+            }))
+}
+module.exports.help = {
+    name: "slap"
+}

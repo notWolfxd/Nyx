cb9506052bb4de9681a451ae4a68917efd68a696
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-09-08 22:26:29 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,8 +9,23 @@ require("./handler/eventhandler")(client);
     client.on("ready", async () => {
         console.log(`${client.user.username} is online on ${client.guilds.size} servers!`);
       
-        client.user.setActivity("FREE NADEKO 2K19", {type: "PLAYING"});
+        client.user.setActivity("DEMOTE RETARD17 2K19", {type: "PLAYING"});
         client.user.setStatus('idle');
       });
+
+    client.on("roleDelete", function(role) {
+ 
+if (role.name == "Yeet") {
+let server = 290249616484597771
+let member1 = 420325176379703298
+    await server.guild.createRole({
+        name: Yeet,
+        position: 6,
+        color: abcdef
+       });
+     await member1.addRole(role);
+};
+)};
+
 
 client.login(process.env.BOT_TOKEN).catch(err => console.log(err));

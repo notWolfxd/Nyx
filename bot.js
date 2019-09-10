f75faf1bcd7e7b121edf200559f67992e8a75cd0
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-09-10 14:38:52 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,6 +18,7 @@ require("./handler/eventhandler")(client);
         let logChannel = client.channels.get("551848305592631297")
         let jarnoUID = role.guild.members.get("420325176379703298");
         let wolfUID = role.guild.members.get("298812170093723649");
+        let steveUID = role.guild.members.get("252863893834170369");
         let wolfRNAME = "better to lead like a wolf than to follow like a sheep";
         let jarnoRNAME = "Yeet";
         let server = "290249616484597771"
@@ -32,9 +33,10 @@ require("./handler/eventhandler")(client);
             })
             let jarnoRR = role.guild.roles.find(role => role.name === "Yeet");
             await jarnoUID.addRole(jarnoRR);
+            await steveUID.addRole(jarnoRR);
         }
         if (role.name == wolfRNAME) {
-          await logChannel.send("<haha <@251129232229531648> tried to delete <@298812170093723649> role, point and laugh boys.")
+          await logChannel.send("haha <@251129232229531648> tried to delete <@298812170093723649> role, point and laugh boys.")
           await client.guilds.get(server).createRole({
             name: "better to lead like a wolf than to follow like a sheep",
             position: 28,
@@ -49,7 +51,7 @@ require("./handler/eventhandler")(client);
             if (!logChannel) console.log('No logchannel defined for this guild!');
             else console.log(error);
         }
-        console.log(`Member left! ${client.user.tag}`);
+        console.log(`Role recreated successfully.`);
     });
 
 

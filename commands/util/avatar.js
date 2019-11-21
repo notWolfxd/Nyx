160006de4f02422d7bf1faf0f660ac7e4884e4cb
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-11-21 14:25:18 -0500

    New member argument handling

--- commands/util/avatar.js
+++ commands/util/avatar.js
@@ -1,19 +1,21 @@
-const Discord = require("discord.js");
+const { RichEmbed } = require("discord.js");
+const { stripIndents } = require("common-tags");
+const {getMember, formatDate} = require("../../functions.js")
 
 module.exports.run = async (client, message, args) => {
 
-     if (message.author.id !== ("298812170093723649") && message.channel.id !== ("410526913879080960")) return;
+    if (message.author.id !== ("298812170093723649") && message.channel.id !== ("410526913879080960")) return;
     let msg = await message.channel.send("Generating avatar...");
 
-    let member = message.mentions.users.first() || message.author
+    const member = getMember(message, args.join(" "));
 
-        let embed = new Discord.RichEmbed()
+        let embed = new RichEmbed()
 
-        .setImage(member.displayAvatarURL)
-        .setColor("fff32e")
-        .setTitle(`${member.tag} Avatar`)
-        .setFooter("Nyx v1.4.7 | Made By: Wolf#9001 | Requested by: " + message.author.tag)
-        .setDescription("[Link]("+member.displayAvatarURL+")");
+        .setImage(member.user.displayAvatarURL)
+        .setColor(member.displayHexColor === '#000000' ? '#ffffff' : member.displayHexColor)
+        .setTitle(`${member.user.tag} Avatar`)
+        .setFooter("Nyx v1.5.0 | Requested by: " + message.author.tag)
+        .setDescription("[Link]("+member.user.displayAvatarURL+")");
 
         message.channel.send(embed)
 
@@ -23,5 +25,5 @@ module.exports.run = async (client, message, args) => {
 
 module.exports.help = {
     name: "avatar",
-    aliases: ["av", "a"]
+    aliases: ["av", "pfp"]
 }

75e5c0f84b04d9e8bb5d3658b9df80f9ada419cd
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-11-23 11:45:58 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,6 @@
-const Discord = require("discord.js");
-const fs = require("fs");
-const config = require("../config.json");
-module.exports = async (client) => {
-
-    client.on("ready", async () => {
+exports.run = (client) => {
         console.log(`${client.user.username} is online on ${client.guilds.size} servers!`);
       
-        client.user.setActivity("FREE NADEKO 2K19", {type: "PLAYING"});
-      });
-
+        client.user.setActivity("DEMOTE RETARDL17 2K19", {type: "PLAYING"});
+        client.user.setStatus('idle');
 }

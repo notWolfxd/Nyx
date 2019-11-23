1ea10eaeea8d2143c6da66cf90433b976994ea6b
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-11-23 12:06:31 -0500

    Update mentionrole.js

--- commands/util/mentionrole.js
+++ commands/util/mentionrole.js
@@ -1,36 +1,27 @@
 const Discord = require("discord.js");
+const {getRole} = require("../../functions.js")
 
 module.exports.run = async (client, message, args) => {
 
 if (!message.member.hasPermission("MANAGE_ROLES"))
    return;
 
-let menrole = args.join(" ");
+const mentionrole = getRole(message, args.join(" "));
 
-if (menrole.position >= message.member.highestRole.position)
+if (!mentionrole.editable)
+        return message.channel.send("I do not have sufficient permissions to mention that role!");
+
+if (mentionrole.position >= message.member.highestRole.position)
         return message.channel.send("You do not have permission to mention this role!");
-        
-let mentionrole;
-        if (message.mentions.roles.size) {
-          mentionrole = message.mentions.roles.first();
-        }
-        else if (message.guild.roles.has(menrole)) {
-          mentionrole = message.guild.roles.get(menrole);
-        }
-        else {
-          mentionrole = message.guild.roles.find(role => role.name.toLowerCase() === menrole.toLowerCase());
-        }
 
-if (!menrole)
-        return message.channel.send("Could not find role to mention.");
+if (!mentionrole)
+        return message.channel.send("Could not find role to mention!")
 
 if (mentionrole.editable) {
   await mentionrole.setMentionable(true, `${message.author.username} | With command mentionrole. (Start)`);
   await message.channel.send(`<@&${mentionrole.id}>`);
   await mentionrole.setMentionable(false, `${message.author.username} | With command mentionrole. (End)`);
         }
-if (!mentionrole.editable)
-        return message.channel.send("I do not have sufficient enough permissions to mention that role.");
 };
 module.exports.help = {
     name: "mentionrole",

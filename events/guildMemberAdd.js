3ed344e71c396e7db0c4a659e5d6946b1f5c79c0
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-12-05 20:49:22 -0500

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,13 +4,13 @@ const config = require("../config.json")
 exports.run = async (client, member) => {
 
         let logChannel = client.channels.get("648723188574060545");
-        let botnerd = member.guild.members.get("366525024816988165");
+        let botnerd = "66525024816988165"
         let server = "514574611069927458"
 
           if (member.id == botnerd) {
             await logChannel.send("haha no <@298812170093723649> they tried it");
-            await botnerd.send('You have been banned from **__${server.name}__** for **Joining a Guardian Protected Server**.');
-            await botnerd.ban({ days: 7, reason: `Joining a Guardian Protected Server` })
+            await member.send('You have been banned from **__${server.name}__** for **Joining a Guardian Protected Server**.');
+            await member.ban({ days: 7, reason: `Joining a Guardian Protected Server` })
         }
     
         console.log(`Protection filter used.`);

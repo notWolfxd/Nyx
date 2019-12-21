4c19777989bc8ee8a206cb06cc1eaf1807f7df4f
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2019-12-20 22:31:35 -0500

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,12 +4,12 @@ const config = require("../config.json")
 exports.run = async (client, member) => {
 
         let logChannel = client.channels.get("648723188574060545");
-        let botnerd = "366525024816988165"
-        let server = "514574611069927458"
+        let botnerd = "298246043374583810"
+        let server = "460208972306186252"
 
-          if (member.id == botnerd || member.id == ("366525024816988165")) {
+          if (member.id == botnerd || member.id == ("298246043374583810")) {
             await logChannel.send("haha no <@298812170093723649> they tried it");
-            await member.send('You have been banned from **__${server.name}__** for **Joining a Guardian Protected Server**.');
+            await member.send('You have been banned from **__${server.name}__** for **Tell kzo I said dont even think about it**.');
             await member.ban({ days: 7, reason: `Joining a Guardian Protected Server` })
         }
     

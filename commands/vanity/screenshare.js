3cb89bfc8069de4b1898e6e3ba46d6229a7c5f46
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-02-08 14:17:24 -0500

    Update screenshare.js

--- commands/vanity/screenshare.js
+++ commands/vanity/screenshare.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const config = require("../../config.json");
 
 module.exports.run = async (client, message, args) => {
 
@@ -13,14 +14,13 @@ module.exports.run = async (client, message, args) => {
     .addField("Description: ", "Request a player to be screenshared by an SS Verified person.")
     .addField("Usage: ", "-ss <IGN> <realm> <reason>")
     .addField("Example: ", "-ss JarnoPwr Demonic Being a nerd.")
-    .setFooter("Nyx v1.4.5 | Made By: Wolf#9001", client.user.avatarURL)
+    .setFooter(`${config.version} | Made By: Wolf#9001`, client.user.avatarURL)
     
 //Define the arguments for Screenshare Request
     let playertoSS = args[0];
     let realm = args[1];
     let reason = args.slice(2).join(" ");
     let ssverifiedrole = message.guild.roles.find(r => r.name === "SS Verified");
-    let member = message.member;
     if (!playertoSS || !realm || !reason) return message.channel.sendEmbed(ssrHelpEmbed);
     
 //Build Screenshare Request message Embed.
@@ -31,7 +31,7 @@ var ssrEmbed = new Discord.RichEmbed()
     .addField("IGN:", `${playertoSS}`, true)
     .addField("Realm:", `${realm}`, true)
     .addField("Reason:", `${reason}`)
-    .setFooter(`Reported By: ${message.author.username} | Nyx v1.4.5 | Made By: Wolf#9001`, client.user.avatarURL)
+    .setFooter(`${config.version} | Reported By: ${message.author.tag} | Made By: Wolf#9001`, client.user.avatarURL)
     
 //Sending the completed command things.
 if (reason) {
@@ -40,7 +40,7 @@ if (reason) {
   await ssverifiedrole.setMentionable(false, `${message.author.username} | With command Screenshare Request. (End)`);
   await client.channels.get("573718629435047956").send(ssrEmbed);
   await message.channel.send("Screenshare staff have been notified with your request.");
- }
+   }
   }
 
 module.exports.help = {

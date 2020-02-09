06ca7ae06f2be1f250f9f01463fea38b3faac24a
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-02-08 20:36:24 -0500

    Update skywarsheads.js

--- commands/hypixel/skywarsheads.js
+++ commands/hypixel/skywarsheads.js
@@ -32,16 +32,16 @@ module.exports.run = async (client, message, args) => {
 
     > **__Heads Total:__** ${player.stats.SkyWars.heads}
     
-    > **Eww:** ${player.stats.SkyWars.heads_eww} | +1 EXP | 0-49 Kills
-    > **Yucky:** ${player.stats.SkyWars.heads_yucky} | +2 EXP | 50-199 Kills
-    > **Meh:** ${player.stats.SkyWars.heads_meh} | +3 EXP | 200-499 Kills
-    > **Decent:** ${player.stats.SkyWars.heads_decent} | +4 EXP | 500-999 Kills
-    > **Salty:** ${player.stats.SkyWars.heads_salty} | +5 EXP | 1000-1999 Kills
-    > **Tasty:** ${player.stats.SkyWars.heads_tasty} | +6 EXP | 2000-4999 Kills
-    > **Succulent:** ${player.stats.SkyWars.heads_succulent} | +7 EXP | 5000-9999 Kills
-    > **Divine:** ${player.stats.SkyWars.heads_divine} | +10 EXP | 10,000-24,999 Kills
-    > **Heavenly:** ${player.stats.SkyWars.heads_heavenly} | +15 EXP | 25,000+ Kills
-    > **Sweet:** ${player.stats.SkyWars.heads_sweet} | +15 EXP | Staff/ YT w/ <10,000 Kills`)
+    > **Eww:** ${(player.stats.SkyWars.heads_eww || 0)} | +1 EXP | 0-49 Kills
+    > **Yucky:** ${(player.stats.SkyWars.heads_yucky || 0)} | +2 EXP | 50-199 Kills
+    > **Meh:** ${(player.stats.SkyWars.heads_meh || 0)} | +3 EXP | 200-499 Kills
+    > **Decent:** ${(player.stats.SkyWars.heads_decent || 0)} | +4 EXP | 500-999 Kills
+    > **Salty:** ${(player.stats.SkyWars.heads_salty || 0)} | +5 EXP | 1000-1999 Kills
+    > **Tasty:** ${(player.stats.SkyWars.heads_tasty || 0)} | +6 EXP | 2000-4999 Kills
+    > **Succulent:** ${(player.stats.SkyWars.heads_succulent || 0)} | +7 EXP | 5000-9999 Kills
+    > **Divine:** ${(player.stats.SkyWars.heads_divine || 0)} | +10 EXP | 10,000-24,999 Kills
+    > **Heavenly:** ${(player.stats.SkyWars.heads_heavenly || 0)} | +15 EXP | 25,000+ Kills
+    > **Sweet:** ${(player.stats.SkyWars.heads_sweet || 0)} | +15 EXP | Staff/ YT w/ <10,000 Kills`)
 
 
     .setFooter(`${config.version} | Requested By: ${message.author.tag}`, client.user.avatarURL)

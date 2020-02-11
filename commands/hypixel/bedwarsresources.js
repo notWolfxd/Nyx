b5a9a432df0107b270e490a7744a621016dcd81d
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-02-11 15:31:21 -0500

    Create bedwarsresources.js

---
+++ commands/hypixel/bedwarsresources.js
@@ -0,0 +1,86 @@
+const { RichEmbed } = require("discord.js");
+const Hypixel = require('hypixel-api');
+const { stripIndents } = require("common-tags");
+const {getMember, formatDate} = require("../../functions.js");
+const config = require("../../config.json")
+
+module.exports.run = async (client, message, args) => {
+
+    
+    if (message.channel.id !== "410526913879080960" && message.channel.id !== "460217052339372042" && message.author.id !== "298812170093723649") return;
+    
+    let member = message.author;
+    let key = process.env.HYPIXEL_TOKEN;
+    const HypixelClient = new Hypixel(key);
+
+    let player;
+
+    try {
+        player = (await HypixelClient.getPlayer('name', args[0])).player
+    }
+    catch (err) {
+        console.log(err)
+        message.channel.send('Hmm, that player doesn\'t seem to exist!')
+        return
+    }
+
+    const embed = new RichEmbed()
+
+    .setDescription(`[**${player.displayname}'s BedWars Stats**](https://hypixel.net/${args[0]})`)
+    .setThumbnail('https://cravatar.eu/head/' + (player.uuid || '') + '?size=2408.png')
+    .setColor(member.displayHexColor === '#000000' ? '#ffffff' : member.displayHexColor)
+
+    .addField('**__Solo:__**', `> **Iron:** ${(player.stats.Bedwars.eight_one_iron_resources_collected_bedwars || 0)}
+    > **Gold:** ${(player.stats.Bedwars.eight_one_gold_resources_collected_bedwars || 0)}
+    > **Diamond:** ${(player.stats.Bedwars.eight_one_diamond_resources_collected_bedwars || 0)}
+    > **Emerald:** ${(player.stats.Bedwars.eight_one_emerald_resources_collected_bedwars || 0)}
+
+    > **Total:** ${(player.stats.Bedwars.eight_one_resources_collected_bedwars || 0)}`, true)
+
+    .addField('**__Doubles:__**', `> **Iron:** ${(player.stats.Bedwars.eight_two_iron_resources_collected_bedwars || 0)}
+    > **Gold:** ${(player.stats.Bedwars.eight_two_gold_resources_collected_bedwars || 0)}
+    > **Diamond:** ${(player.stats.Bedwars.eight_two_diamond_resources_collected_bedwars || 0)}
+    > **Emerald:** ${(player.stats.Bedwars.eight_two_emerald_resources_collected_bedwars || 0)}
+
+    > **Total:** ${(player.stats.Bedwars.eight_two_resources_collected_bedwars || 0)}`, true)
+
+    .addField('➖➖➖', `➖➖➖`)
+
+    .addField('**__3v3v3v3:__**', `> **Iron:** ${(player.stats.Bedwars.four_three_iron_resources_collected_bedwars || 0)}
+    > **Gold:** ${(player.stats.Bedwars.four_three_gold_resources_collected_bedwars || 0)}
+    > **Diamond:** ${(player.stats.Bedwars.four_three_diamond_resources_collected_bedwars || 0)}
+    > **Emerald:** ${(player.stats.Bedwars.four_three_emerald_resources_collected_bedwars || 0)}
+    
+    > **Total:** ${(player.stats.Bedwars.four_three_resources_collected_bedwars || 0)}`, true)
+
+    .addField('**__4v4v4v4:__**', `> **Iron:** ${(player.stats.Bedwars.four_four_iron_resources_collected_bedwars || 0)}
+    > **Gold:** ${(player.stats.Bedwars.four_four_gold_resources_collected_bedwars || 0)}
+    > **Diamond:** ${(player.stats.Bedwars.four_four_diamond_resources_collected_bedwars || 0)}
+    > **Emerald:** ${(player.stats.Bedwars.four_four_emerald_resources_collected_bedwars || 0)}
+    
+    > **Total:** ${(player.stats.Bedwars.four_four_resources_collected_bedwars || 0)}`, true)
+
+    .addField('➖➖➖', `➖➖➖`)
+
+    .addField('**__Overall:__**', `> **Iron:** ${(player.stats.Bedwars.iron_resources_collected_bedwars || 0)}
+    > **Gold:** ${(player.stats.Bedwars.gold_resources_collected_bedwars || 0)}
+    > **Diamond:** ${(player.stats.Bedwars.diamond_resources_collected_bedwars || 0)}
+    > **Emerald:** ${(player.stats.Bedwars.emerald_resources_collected_bedwars || 0)}
+    
+    > **Total:** ${(player.stats.Bedwars.resources_collected_bedwars || 0)}`, true)
+
+    .setFooter(`${config.version} | Requested By: ${message.author.tag}`, client.user.avatarURL)
+
+    message.channel.send(embed)
+
+}
+
+module.exports.help = {
+    name: "bedwarsresources",
+    aliases: [
+        "bwr",
+        "bwresources",
+        "bwres",
+        "bwrescollected"
+    ]
+}

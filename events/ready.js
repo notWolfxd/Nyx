ef3e8572ae739ebe2b52d35bf3bafb4f09bcb7e5
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-02-12 19:09:15 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 exports.run = (client) => {
         console.log(`${client.user.username} is online on ${client.guilds.size} servers!`);
       
-        client.user.setActivity("DEMOTE RETARDL17 2K19", {type: "PLAYING"});
+        client.user.setActivity("Hypixel Cmds = WIP, report bugs thx", {type: "PLAYING"});
         client.user.setStatus('idle');
 }

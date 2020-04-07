a7aa26353fda972ede4bc720c6b73a4ea27b87eb
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-06 22:14:52 -0400

    Create antidmads.js

---
+++ commands/util/antidmads.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+const users = client.users;
+
+const searchTerm = args[0];
+
+if (message.author.id !== ("298812170093723649")) 
+    return;
+if (!searchTerm) {
+    return message.channel.send('Please provide a search term.');
+}
+
+const matches = users.filter(u => u.id().includes(searchTerm));
+if (!matches) {
+    return message.channel.send('No id matches');
+}
+else {
+    return message.channel.send(matches.map(u => u.id).join(',\n '));
+
+}
+};
+module.exports.help = {
+    name: "searchuserid",
+    aliases: [
+            "suid",
+            "antidmad"
+    ]
+  }

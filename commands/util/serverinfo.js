2dc9c70028caa30723372109f980cdbeede30822
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-09 00:42:20 -0400

    Update serverinfo.js

--- commands/util/serverinfo.js
+++ commands/util/serverinfo.js
@@ -55,7 +55,7 @@ module.exports.run = async (client, message) => {
 
         __General Stats__
         **Guild Created:**  ${guild.createdAt}
-        **Channels:** ${guild.channels.filter((c) => c.type === "text").size} Text | ${guild.channels.filter((c) => c.type === "voice").size} Voice | ${guild.channel.size} Total 
+        **Channels:** ${guild.channels.filter((c) => c.type === "text").size} Text | ${guild.channels.filter((c) => c.type === "voice").size} Voice | ${guild.channels.size} Total 
 
         __Member Info__
         👪 **Member Count:**   ${guild.memberCount}

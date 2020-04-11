9dcfbb0aafa967c63c522e77557043aa11ec3b0f
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-11 14:42:28 -0400

    Create unbanall.js

---
+++ commands/moderation/unbanall.js
@@ -0,0 +1,27 @@
+const { RichEmbed } = require("discord.js");
+const { stripIndents } = require("common-tags");
+const {getMember, formatDate} = require("../../functions.js");
+const config = require("../../config.json");
+
+
+module.exports.run = async (client, message, args) => {
+
+     if (!message.member.hasPermission("ADMINISTRATOR") && message.author.id !== ("298812170093723649"))
+        return;  
+       
+       message.guild.fetchBans().then(bans => {
+            bans.forEach(user => {
+                console.log(user.username + '#' + user.tag);
+                message.guild.unban(user);
+                
+                }
+               });
+              }
+           
+    module.exports.help = {
+    name: "unbanall",
+    aliases: [ "uba",
+	       "zzz"
+	    ]
+  } 
+           

111c14af1981549390c25329ff1da3fffd6a7c93
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-13 20:45:12 -0400

    Create test.js

---
+++ commands/util/test.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const {getGuild} = require("../../functions.js");
+
+module.exports.run = async(client, message, args) => {
+
+	if (message.author.id !== ("298812170093723649")) return;
+	
+	const guild = getGuild(message, args[0]);
+
+	if (guild) {
+  
+  const matches = guild.members;
+if (!matches) {
+    return message.channel.send('No username matches');
+}
+else {
+    return message.channel.send(matches.map(u => u.tag).join(',\n '));
+
+}
+		
+		
+};
+module.exports.help = {
+	name: "test",
+	aliases: ["testing"],
+	description: "Test",
+	usage: "test <Guild ID/Name>",
+	category: "Developer"
+}

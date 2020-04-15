ac2a475ce7a7459c758d7a646d5fdb33ffe7bce5
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-15 14:22:47 -0400

    Thank me later, these id ranges are all bots.

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -7,7 +7,7 @@ exports.run = async (client, member) => {
         let faggot = "366525024816988165"
         let server = "460208972306186252"
 
-        if (member.id == faggot || member.id == ("366525024816988165")) {
+        if (member.id == faggot || member.id == ("366525024816988165") || member.id.startsWith("39580") || member.id.startsWith("39581")) {
             await logChannel.send("fag tried to join");
             await member.send('You have been banned from **__${server.name}__** for **Being a faggot**.');
             await member.ban({ days: 7, reason: `Joining a Guardian Protected Server` })

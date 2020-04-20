6b43df3d99882db11210dc8fa842f8e1b33c02a6
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-19 23:05:58 -0400

    Update lewdneko.js

--- commands/anime/lewdneko.js
+++ commands/anime/lewdneko.js
@@ -1,15 +1,28 @@
 const Discord = require("discord.js");
-const snekfetch = require("snekfetch")
+const snekfetch = require("snekfetch");
+const config = require("../../config.json");
+
 module.exports.run = async (client, message, args) => {
+    
     if (message.author.id !== ("298812170093723649") && message.channel.id !== ("410526913879080960") && message.channel.id !== ("541342157408043071") && message.channel.id !== ("324056323794796544")) return;
-    if (!message.channel.nsfw) return;
-    const { body } = await snekfetch
-       .get('https://nekos.life/api/lewd/neko');
-       const embed = new Discord.RichEmbed()
-       .setColor(0x00A2E8)
-       .setImage(body.neko)
-       message.channel.send(embed)
+   
+      if (!message.channel.nsfw) return;
+    
+        const { body } = await snekfetch
+         .get('https://nekos.life/api/lewd/neko');
+    
+        let embed = new Discord.RichEmbed()
+        .setTimestamp()
+        .setColor(0x00A2E8)
+        .setImage(body.neko)
+        .setFooter(`${config.version} | Requested By: ${message.author.tag}`)
+        
+       message.channel.send(embed);
  }
 module.exports.help = {
-    name: "lewdneko"
+    name: "lewdneko",
+    aliases: [ "nsfwneko" ],
+    description: "Show an image of a random NSFW neko girl.",
+    Usage: "lewdneko",
+    Category: "Anime"
 }

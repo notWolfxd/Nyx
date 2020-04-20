7ab0880099b423dfaffd7dc3c6865930e35de070
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-20 00:10:18 -0400

    Create baka.js

---
+++ commands/anime/baka.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const snekfetch = require("snekfetch");
+const { getMember } = require("../../functions.js");
+
+module.exports.run = async (client, message, args) => {
+
+    if (message.author.id !== ("298812170093723649") && message.author.id !== ("216416135929790464") && message.channel.id !== ("410526913879080960") && message.channel.id !== ("324056323794796544")) return;
+      
+    if (!args[0]) return message.channel.send("no u.");
+    
+      const user = getMember(message, args[0]);
+    
+        snekfetch.get('https://nekos.life/api/v2/img/baka')
+            .set('Key', 'dnZ4fFJbjtch56pNbfrZeSRfgWqdPDgf')
+            .then(r => message.channel.send(`${user}, you were called an IDIOT by ${message.author.username}!`,{
+                embed: {
+                    image: {
+                        url: r.body.url
+                    }
+                }
+            }))
+}
+module.exports.help = {
+    name: "baka",
+    aliases: [ "isiot" ],
+    description: "Call someone an idiot.",
+    usage: "bqka <User>",
+    example: "baka @ProZed",
+    category: "Anime"
+}

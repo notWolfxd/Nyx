94fc5a6ed637f32dc0498afe96c71272f8a077a7
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-20 17:47:32 -0400

    Create nekogif.js

---
+++ commands/anime/nekogif.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const snekfetch = require("snekfetch");
+const config = require("../../config.json");
+
+module.exports.run = async (client, message, args) => {
+    
+    if (message.author.id !== ("298812170093723649") && message.channel.id !== ("410526913879080960") && message.channel.id !== ("541342157408043071") && message.channel.id !== ("324056323794796544")) return;
+   
+      if (message.author.id !== ("298812170093723649") && message.channel.id !== ("410526913879080960") && message.guild.id !== ("460208972306186252") && message.channel.id !== ("517488355693559818") && message.channel.id !== ("324056323794796544")) return;
+    
+        const { body } = await snekfetch
+         .get('https://nekos.life/api/v2/img/ngif');
+    
+        let embed = new Discord.RichEmbed()
+        .setTimestamp()
+        .setColor(0x00A2E8)
+        .setImage(body.url)
+        .setFooter(`${config.version} | Requested By: ${message.author.tag}`)
+        
+       message.channel.send(embed);
+ }
+module.exports.help = {
+    name: "nekogif",
+    aliases: [ "ngif" ],
+    description: "Show a gif of a random neko anime girl.",
+    usage: "nekogif",
+    category: "Anime"
+}

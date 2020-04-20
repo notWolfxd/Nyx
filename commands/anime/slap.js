a8b28915f54c0db656df503e1df0c78370258dc2
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-19 23:25:14 -0400

    Update slap.js

--- commands/anime/slap.js
+++ commands/anime/slap.js
@@ -1,10 +1,15 @@
 const Discord = require("discord.js");
-const snekfetch = require("snekfetch")
+const snekfetch = require("snekfetch");
+const { getMember } = require("../../functions.js");
+
 module.exports.run = async (client, message, args) => {
 
     if (message.author.id !== ("298812170093723649") && message.author.id !== ("216416135929790464") && message.channel.id !== ("410526913879080960") && message.channel.id !== ("324056323794796544")) return;
-      if (message.mentions.users.size < 1) return message.channel.send("You missed your target I assume.")
-      let user = message.guild.member(message.mentions.users.first());
+      
+    if (!args[0]) return message.channel.send("You missed your target I assume.");
+    
+      const user = getMember(message, args[0]);
+    
         snekfetch.get('https://nekos.life/api/v2/img/slap')
             .set('Key', 'dnZ4fFJbjtch56pNbfrZeSRfgWqdPDgf')
             .then(r => message.channel.send(`${user}, you were slapped by ${message.author.username}!`,{
@@ -16,5 +21,10 @@ module.exports.run = async (client, message, args) => {
             }))
 }
 module.exports.help = {
-    name: "slap"
+    name: "slap",
+    aliases: [ "smack" ],
+    description: "Slap someone.",
+    usage: "slap <User>",
+    example: "slap @UrMa",
+    category: "Anime"
 }

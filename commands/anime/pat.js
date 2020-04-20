3c4d36f6da61757c604c5cf4878e72684b2bbb58
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-19 23:21:47 -0400

    Update pat.js

--- commands/anime/pat.js
+++ commands/anime/pat.js
@@ -1,9 +1,15 @@
 const Discord = require("discord.js");
-const snekfetch = require("snekfetch")
+const snekfetch = require("snekfetch");
+const { getMember } = require("../../functions.js");
+
 module.exports.run = (client, message, args) => {
+    
     if (message.author.id !== ("298812170093723649") && message.author.id !== ("216416135929790464") && message.channel.id !== ("410526913879080960") && message.channel.id !== ("324056323794796544")) return;
-      if (message.mentions.users.size < 1) return message.channel.send("You can't head pat the air (it's just not as satisfying).")
-      let user = message.guild.member(message.mentions.users.first());
+      
+    if (!args[0]) return message.channel.send("You can't head pat the air (it's just not as satisfying).");
+    
+      const user = getMember(message, args[0]);
+    
         snekfetch.get('https://nekos.life/api/pat')
             .set('Key', 'dnZ4fFJbjtch56pNbfrZeSRfgWqdPDgf')
             .then(r => message.channel.send(`${user}, you got a head pat from ${message.author.username} ❤`,{
@@ -15,5 +21,10 @@ module.exports.run = (client, message, args) => {
             }))
 }
 module.exports.help = {
-    name: "pat"
+    name: "pat",
+    aliases: [ "headpat" ],
+    description: "Pat someone on the head.",
+    usage: "pat <User>",
+    example: "pat @Wolf",
+    category: "Anime"
 }

1380031ab1e3c4ba64f5d655995552d0410dd398
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-20 17:04:07 -0400

    Create pwankgif.js

---
+++ commands/nsfw/pwankgif.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const snekfetch = require("snekfetch");
+const config = require("../../config.json");
+
+module.exports.run = async (client, message, args) => {
+    
+    if (message.author.id !== ("298812170093723649") && message.channel.id !== ("410526913879080960") && message.channel.id !== ("541342157408043071") && message.channel.id !== ("324056323794796544")) return;
+   
+      if (!message.channel.nsfw) return;
+    
+        const { body } = await snekfetch
+         .get('https://nekos.life/api/v2/img/pwankg');
+    
+        let embed = new Discord.RichEmbed()
+        .setTimestamp()
+        .setColor(0x00A2E8)
+        .setImage(body.url)
+        .setFooter(`${config.version} | Requested By: ${message.author.tag}`)
+        
+       message.channel.send(embed);
+ }
+module.exports.help = {
+    name: "pwankgif",
+    aliases: [ "pwankg", "wankgif" ],
+    description: "Show a gif of a random anime girl wanking.",
+    usage: "pwankgif",
+    category: "NSFW"
+}

cbb4314e5dd9276ee92cded44d303cd6a12c6c54
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-19 22:56:09 -0400

    Update kiss.js

--- commands/anime/kiss.js
+++ commands/anime/kiss.js
@@ -1,9 +1,15 @@
 const Discord = require("discord.js");
-const snekfetch = require("snekfetch")
+const snekfetch = require("snekfetch");
+const { getMember } = require("../../functions.js");
+
 module.exports.run = async (client, message, args) => {
+    
     if (message.author.id !== ("298812170093723649") && message.author.id !== ("216416135929790464") && message.channel.id !== ("410526913879080960") && message.channel.id !== ("324056323794796544")) return;
-      if (message.mentions.users.size < 1) return message.channel.send("You can't kiss the air (well you can but you're gay if you do).")
-      let user = message.guild.member(message.mentions.users.first());
+      
+    if (!args[0]) return message.channel.send("You can't kiss the air (well you can but you're gay if you do).");
+    
+      const user = getMember(message, args[0]);
+    
         snekfetch.get('https://nekos.life/api/kiss')
             .set('Key', 'dnZ4fFJbjtch56pNbfrZeSRfgWqdPDgf')
             .then(r => message.channel.send(`${user}, you got a kiss from ${message.author.username} ❤`,{
@@ -12,8 +18,13 @@ module.exports.run = async (client, message, args) => {
                         url: r.body.url
                     }
                 }
-            })).catch(console.error);
+            }))
 }
 module.exports.help = {
-    name: "kiss"
+    name: "kiss",
+    aliases: [ "kissu", "kisses" ],
+    description: "Kiss someone.",
+    Usage: "kiss <User>",
+    Example: "kiss @Bestle",
+    Category: "Anime"
 }

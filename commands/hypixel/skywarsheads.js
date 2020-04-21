50f95de4b561d3f9ddc51c598e29d7b0f9e6855c
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-20 22:44:59 -0400

    Update skywarsheads.js

--- commands/hypixel/skywarsheads.js
+++ commands/hypixel/skywarsheads.js
@@ -1,27 +1,26 @@
 const { RichEmbed } = require("discord.js");
 const Hypixel = require('hypixel-api');
 const { stripIndents } = require("common-tags");
-const {getMember, formatDate} = require("../../functions.js");
 const config = require("../../config.json")
 
 module.exports.run = async (client, message, args) => {
 
-    if (message.channel.id !== "410526913879080960" && message.channel.id !== "460217052339372042" && message.author.id !== "298812170093723649") return;
+  if (message.channel.id !== "410526913879080960" && message.channel.id !== "460217052339372042" && message.author.id !== "298812170093723649") return;
 
     let member = message.author;
     let key = process.env.HYPIXEL_TOKEN;
-    const HypixelClient = new Hypixel(key);
-
     let player;
 
+    const HypixelClient = new Hypixel(key);
+    
     try {
         player = (await HypixelClient.getPlayer('name', args[0])).player
-    }
+        }
     catch (err) {
         console.log(err)
         message.channel.send('Hmm, that player doesn\'t seem to exist!')
-        return
-    }
+        return;
+                }
 
     const embed = new RichEmbed()
 
@@ -31,8 +30,8 @@ module.exports.run = async (client, message, args) => {
 
     .addField('**__Heads Information:__**', stripIndents`Each head gives a different amount of EXP, and can only be obtained from corrupted games.
 
-    > **__Angel of Death Tier:__** ${(player.stats.SkyWars.angel_of_death_level) || 0}
-    > **__Heads Total:__** ${(player.stats.SkyWars.heads || 0)}
+    > **Angel of Death Tier:** ${(player.stats.SkyWars.angel_of_death_level || 0)}
+    > **Heads Total:** ${(player.stats.SkyWars.heads || 0)}
     
     > **Eww:** ${(player.stats.SkyWars.heads_eww || 0)} | +1 EXP | 0-49 Kills
     > **Yucky:** ${(player.stats.SkyWars.heads_yucky || 0)} | +2 EXP | 50-199 Kills
@@ -45,17 +44,17 @@ module.exports.run = async (client, message, args) => {
     > **Heavenly:** ${(player.stats.SkyWars.heads_heavenly || 0)} | +15 EXP | 25,000+ Kills
     > **Sweet:** ${(player.stats.SkyWars.heads_sweet || 0)} | +15 EXP | Staff/ YT w/ <10,000 Kills`)
 
-
     .setFooter(`${config.version} | Requested By: ${message.author.tag}`, client.user.avatarURL)
 
-    message.channel.send(embed)
+    message.channel.send(embed);
 
 }
 
 module.exports.help = {
     name: "skywarsheads",
-    aliases: [
-        "swheads",
-        "swh"
-    ]
+    aliases: [ "swheads", "swh" ],
+    description: "Get a player's SkyWars corrupted game head statistics.",
+    usage: "skywarsheads <IGN>",
+    example: "skywarsheads Childish",
+    category: "Hypixel"
 }

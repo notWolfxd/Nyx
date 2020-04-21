0b4bc0c9e5beb3f87852f94bfe0206acda07bc29
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-20 22:36:57 -0400

    Update bedwarsstats.js

--- commands/hypixel/bedwarsstats.js
+++ commands/hypixel/bedwarsstats.js
@@ -1,28 +1,27 @@
 const { RichEmbed } = require("discord.js");
 const Hypixel = require('hypixel-api');
 const { stripIndents } = require("common-tags");
-const {getMember, formatDate} = require("../../functions.js");
 const config = require("../../config.json")
 
 module.exports.run = async (client, message, args) => {
 
     
-    if (message.channel.id !== "410526913879080960" && message.channel.id !== "460217052339372042" && message.author.id !== "298812170093723649") return;
+  if (message.channel.id !== "410526913879080960" && message.channel.id !== "460217052339372042" && message.author.id !== "298812170093723649") return;
     
     let member = message.author;
     let key = process.env.HYPIXEL_TOKEN;
-    const HypixelClient = new Hypixel(key);
-
     let player;
 
+    const HypixelClient = new Hypixel(key);
+    
     try {
-        player = (await HypixelClient.getPlayer('name', args[0])).player
-    }
+        player = ( await HypixelClient.getPlayer('name', args[0])).player
+        }
     catch (err) {
         console.log(err)
         message.channel.send('Hmm, that player doesn\'t seem to exist!')
-        return
-    }
+        return;
+                }
 
     const embed = new RichEmbed()
 
@@ -30,14 +29,14 @@ module.exports.run = async (client, message, args) => {
     .setThumbnail('https://cravatar.eu/head/' + (player.uuid || '') + '?size=2408.png')
     .setColor(member.displayHexColor === '#000000' ? '#ffffff' : member.displayHexColor)
 
-    .addField('**__General:__**', `:rosette: **Level:** ${(player.achievements.bedwars_level) || 0}
+    .addField('**__General:__**', stripIndents`:rosette: **Level:** ${(player.achievements.bedwars_level || 0)}
     🏆 **Winstreak:** ${(player.stats.Bedwars.winstreak || 0)}
-    :video_game: **Games Played:** ${player.stats.Bedwars.games_played_bedwars || 0}
+    :video_game: **Games Played:** ${(player.stats.Bedwars.games_played_bedwars || 0)}
 
     :moneybag: **Coins:** ${(player.stats.Bedwars.coins || 0)}
     🎁 **Loot Boxes:** ${((player.stats.Bedwars.bedwars_boxes || 0) + (player.stats.Bedwars.bedwars_halloween_boxes || 0) + (player.stats.Bedwars.bedwars_lunar_boxes || 0) + (player.stats.Bedwars.bedwars_christmas_boxes || 0) + (player.stats.Bedwars.bedwars_easter_boxes || 0) || 0)}`)
 
-    .addField('**__Solo:__**', `> **Beds Broken:** ${(player.stats.Bedwars.eight_one_beds_broken_bedwars || 0)}
+    .addField('**__Solo:__**', stripIndents`> **Beds Broken:** ${(player.stats.Bedwars.eight_one_beds_broken_bedwars || 0)}
     
     > **Kills:** ${(player.stats.Bedwars.eight_one_kills_bedwars || 0)}
     > **Deaths:** ${(player.stats.Bedwars.eight_one_deaths_bedwars || 0)}
@@ -51,7 +50,7 @@ module.exports.run = async (client, message, args) => {
     > **Final Deaths:** ${(player.stats.Bedwars.eight_one_final_deaths_bedwars || 0)}
     > **FK/FD:** ${((player.stats.Bedwars.eight_one_final_kills_bedwars || 0) / (player.stats.Bedwars.eight_one_final_deaths_bedwars || 1)).toPrecision(2)}`, true)
 
-    .addField('**__Doubles:__**', `> **Beds Broken:** ${(player.stats.Bedwars.eight_two_beds_broken_bedwars || 0)}
+    .addField('**__Doubles:__**', stripIndents`> **Beds Broken:** ${(player.stats.Bedwars.eight_two_beds_broken_bedwars || 0)}
     
     > **Kills:** ${(player.stats.Bedwars.eight_two_kills_bedwars || 0)}
     > **Deaths:** ${(player.stats.Bedwars.eight_two_deaths_bedwars || 0)}
@@ -67,7 +66,7 @@ module.exports.run = async (client, message, args) => {
 
     .addField('➖➖➖', `➖➖➖`)
 
-    .addField('**__3v3v3v3:__**', `> **Beds Broken:** ${(player.stats.Bedwars.four_three_beds_broken_bedwars || 0)}
+    .addField('**__3v3v3v3:__**', stripIndents`> **Beds Broken:** ${(player.stats.Bedwars.four_three_beds_broken_bedwars || 0)}
     
     > **Kills:** ${(player.stats.Bedwars.four_three_kills_bedwars || 0)}
     > **Deaths:** ${(player.stats.Bedwars.four_three_deaths_bedwars || 0)}
@@ -81,7 +80,7 @@ module.exports.run = async (client, message, args) => {
     > **Final Deaths:** ${(player.stats.Bedwars.four_three_final_deaths_bedwars || 0)}
     > **FK/FD:** ${((player.stats.Bedwars.four_three_final_kills_bedwars || 0) / (player.stats.Bedwars.four_three_final_deaths_bedwars || 1)).toPrecision(2)}`, true)
 
-    .addField('**__4v4v4v4:__**', `> **Beds Broken:** ${(player.stats.Bedwars.four_four_beds_broken_bedwars || 0)}
+    .addField('**__4v4v4v4:__**', stripIndents`> **Beds Broken:** ${(player.stats.Bedwars.four_four_beds_broken_bedwars || 0)}
     
     > **Kills:** ${(player.stats.Bedwars.four_four_kills_bedwars || 0)}
     > **Deaths:** ${(player.stats.Bedwars.four_four_deaths_bedwars || 0)}
@@ -97,7 +96,7 @@ module.exports.run = async (client, message, args) => {
 
     .addField('➖➖➖', `➖➖➖`)
 
-    .addField('**__Overall:__**', `> **Beds Broken:** ${(player.stats.Bedwars.beds_broken_bedwars || 0)}
+    .addField('**__Overall:__**', stripIndents`> **Beds Broken:** ${(player.stats.Bedwars.beds_broken_bedwars || 0)}
     
     > **Kills:** ${(player.stats.Bedwars.kills_bedwars || 0)}
     > **Deaths:** ${(player.stats.Bedwars.deaths_bedwars || 0)}
@@ -113,16 +112,15 @@ module.exports.run = async (client, message, args) => {
 
     .setFooter(`${config.version} | Requested By: ${message.author.tag}`, client.user.avatarURL)
 
-    message.channel.send(embed)
+    message.channel.send(embed);
 
 }
 
 module.exports.help = {
     name: "bedwarsstats",
-    aliases: [
-        "bw",
-        "bwstats",
-        "bedwars",
-        "bws"
-    ]
+    aliases: [ "bw", "bwstats", "bedwars", "bws" ],
+    description: "Get a player's BedWars general statistics.",
+    usage: "bedwarsstats <IGN>",
+    example: "bedwarsstats Wolf",
+    category: "Hypixel"
 }

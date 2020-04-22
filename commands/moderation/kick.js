8d9faadba2464db0ee59fab428a819be84dd9bc3
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-21 22:27:02 -0400

    Update kick.js

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -5,66 +5,55 @@ const config = require("../../config.json");
 
 module.exports.run = async (client, message, args) => {
 
-    const member = getMember(message, args[0]);
-    let modlog = message.guild.channels.find(channel => channel.name === "bot-logs");
-    let reason = args.splice(1, args.length).join(' ') || `No reason provided.`;
+const member = getMember(message, args[0]);
+	
+  let modlog = message.guild.channels.find(channel => channel.name === "bot-logs");
+  let reason = args.splice(1, args.length).join(' ') || `No reason provided.`;
 
-//A whole lot of ifs to protect stuff.
-if (!message.member.hasPermission("KICK_MEMBERS")) 
+if (!message.member.hasPermission("KICK_MEMBERS"))
         return;
-
 if (!args[0]) 
-        return message.channel.send("I can kick air, but I'd prefer to take it out on a person.");
-
+        return message.channel.send("I can't kick air.");
 if (member.id === message.author.id)
-        return message.channel.send("You cannot kick yourself.")
-
+        return message.channel.send("You can't kick yourself.");
 if (member.id === "298812170093723649")
-        return message.channel.send("You cannot kick Wolf.")
-
+        return message.channel.send("You can't kick Wolf.");
 if (!member.kickable)
-        return message.channel.send("Something has stopped me from kicking them (higher role maybe?).");
-
+        return message.channel.send("Something has prevented me from kicking them.");
 if (member.highestRole.position === message.member.highestRole.position)
-        return message.channel.send("Play nice with the people on your level.")
-
+        return message.channel.send("Play nice with the people on your level.");
 if (member.highestRole.position > message.member.highestRole.position)
-        return message.channel.send("You cannot kick your superiors.")
+        return message.channel.send("I guess I see why they are a higher position than you.");
 
-    member.send(`You have been kicked from **__${message.guild.name}__** by **__${message.author.tag}__** for: **__${reason}__**.`)
-    member.kick(`${message.author.tag} | For: ${reason}`).then(() => {
+    member.send(`You have been kicked from **__${message.guild.name}__** by **__${message.author.username}__** for: **__${reason}__**.`);
+    member.kick({ reason: `${message.author.tag} | For: ${reason}` });
         
-var kEmbed = new RichEmbed()
-        .setTitle("⚠️ User Kicked")
-        .setColor("#32CD32")
-        .addField("Username", `${member.user.tag}`, true)
-        .addField("ID", `${member.user.id}`, true)
-message.channel.send(kEmbed);
-        
-var embed = new RichEmbed()
-        .setTitle('User Kicked')
-        .setColor("#8B0000")
-        .setThumbnail(message.author.avatarURL)
-	.addField(`User:`, `${member.user.tag} | ${member.user.id}`)
-	.addField(`Issued By:`, `${message.author.tag} | ${message.author.id}`)
-	.addField(`Reason:`, `${reason}`)
-        .setFooter(`${config.version} | Made By: Wolf#9001`, client.user.avatarURL)
-if (modlog) modlog.send({ embed })
+const kickedEmbed = new RichEmbed()
+     .setTitle("⚠️ User Kicked")
+     .setColor("#32CD32")
+     .addField("Username", `${member.user.tag}`, true)
+     .addField("ID", `${member.user.id}`, true)
 
-	.then(() => {
-		client.log('log', `${message.guild.name}/#${message.channel.name} (${message.channel.id}): ${member.user.tag} (${member.user.id}) was kicked by ${message.author.tag} (${message.author.id})`, 'CMD');
-		})
-	.catch((err) => {
-		console.log(err);
-		});
-	})
-	.catch(error => message.channel.send(`Unable to kick ${member} because of : ${error}`));
+    message.channel.send(kickedEmbed);
 
-};
+const logEmbed = new RichEmbed()
+     .setTitle('User Kicked')
+     .setColor("#8B0000")
+     .setTimestamp()
+     .setThumbnail(message.author.avatarURL)
+     .addField(`**__MEMBER KICKED__**`, stripIndents`> **User Kicked:** ${member.user.tag} | ${member.user.id}
+     > **Issued By:** ${message.author.tag} | ${message.author.id}
+     > **Reason:** ${reason}`)
+     .setFooter(`${config.version} | Made By: Wolf#9001`, client.user.avatarURL)
+
+    if (modlog) modlog.send({ logEmbed });
+                        
+}
 module.exports.help = {
-    name: "k",
-    aliases: [
-            "kick",
-            "cyalater"
-    ]
+    name: "kick",
+    aliases: [ "k", "cyalater" ],
+    description: "Kick a user from the guild.",
+    usage: "kick <User> [Reason]",
+    example: "kick GlennL17 simp",
+    category: "Moderation"
   }

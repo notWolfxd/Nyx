95bf02fc7530d2764748ef9239c0f579e0886fd4
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-22 18:06:28 -0400

    Update help.js

--- commands/util/help.js
+++ commands/util/help.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
 
         const data = [];
-        const { commands } = message.client;
+        const { commands } = client.commands;
 
         if (!args.length) {
             data.push('Here\'s a list of all my commands:');

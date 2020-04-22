74426738d423500caecd2c5876712f85ad824223
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-22 02:17:12 -0400

    Create rockpaperscissors.js

---
+++ commands/fun/rockpaperscissors.js
@@ -0,0 +1,43 @@
+const { Discord } = require("discord.js");
+const config = require("../../config.json");
+
+module.exports.run = async (client, message, args) => {
+
+if (message.author.id !== ("298812170093723649") && message.channel.id !== ("410526913879080960") && message.guild.id !== ("460208972306186252") && message.channel.id !== ("517488355693559818") && message.channel.id !== ("324056323794796544")) return;
+
+    const choices = [ "rock", "paper", "scissors" ];
+    const response = choices[Math.floor(Math.random() * choices.length)];
+    const choice = args[0].toLowerCase();
+  
+  if (!choices.includes(choice.toLowerCase())
+     return message.channel.send(`${message.author.username}, that is not a valid choice, idiot.`);
+     
+  if (choice == response) 
+     return message.channsl.send(`🔫 ${message.author.username}, we both chose ${choice}. It is a **tie**!`);
+     
+  else if (choice !== response) {
+  
+    if (choice == ("rock") && response == ("paper"))
+       return message.channel.send(`🗒 ${message.author.username}, I chose \`Paper\`, you **lose**!`);
+    if (choice == ("rock") && response == ("scissors"))
+       return message.channel.send(`✂️ ${message.author.username}, I chose \`Scissors\`, you **win**!`);
+    if (choice == ("scissorz") && response == ("paper"))
+       return message.channel.send(`🗒 ${message.author.username}, I chose \`Paper\`, you **win**!`);
+    if (choice == ("scissors") && response == ("rock"))
+       return message.channel.send(`🎐 ${message.author.username}, I chose \`Rock\`, you **lose**!`);
+    if (choice == ("paper") && response == ("scissors"))
+       return message.channel.send(`✂️ ${message.author.username}, I chose \`Scissors\`, you **lose**!`);
+    if (choice == ("paper") && response == ("rock"))
+       return message.channel.send(`🎐 ${message.author.username}, I chose \`Rock\`, you **win**!`);
+           }
+           
+}
+module.exports.help = {
+    name: "rockpaperscissors",
+    aliases: [ "rps" ],
+    description: "Play a round of rock paper scissors with the bot.",
+    usage: "rockpaperscissors <Choice>",
+    example: "rockpaperscissors rock",
+    category: "Fun"
+
+}

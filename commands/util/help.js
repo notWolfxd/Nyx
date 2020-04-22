383e1086d1030247e11b871ee58d06f77eb389d3
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-22 18:01:54 -0400

    Create help.js

---
+++ commands/util/help.js
@@ -0,0 +1,49 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+        const data = [];
+        const { commands } = message.client;
+
+        if (!args.length) {
+            data.push('Here\'s a list of all my commands:');
+            data.push(commands.map(command => command.name).join(', '));
+            data.push(`\nYou can send \`-help [command name]\` to get info on a specific command!`);
+
+            return message.author.send(data, { split: true })
+                .then(() => {
+                    if (message.channel.type == 'dm') return;
+                    message.reply('I\'ve sent you a DM with all my commands!');
+                })
+                .catch(error => {
+                    console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
+                    message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
+                });
+        }
+
+        const name = args[0].toLowerCase();
+		const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+
+		if (!command) {
+			return message.reply('that\'s not a valid command!');
+		}
+
+		data.push(`**Name:** ${command.name}`);
+
+		if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+		if (command.description) data.push(`**Description:** ${command.description}`);
+
+		if (command.usage) data.push(`**Usage:** ${command.name} ${command.usage}`);
+    if (command.example) data.push(`**Example:** ${command.example}
+		if (command.category) data.push(`**Category:** ${command.cooldown}`);
+
+		message.channel.send(data, { split: true });
+    }
+
+    module.exports.help = {
+    name: "help",
+    aliases: [
+            "h",
+            "cmd"
+    ]
+  }

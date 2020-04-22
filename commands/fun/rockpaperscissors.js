eb5ecc2720b6c37c042e5f9249a5c39fe0173f6e
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-22 03:03:50 -0400

    Update rockpaperscissors.js

--- commands/fun/rockpaperscissors.js
+++ commands/fun/rockpaperscissors.js
@@ -8,7 +8,7 @@ if (message.author.id !== ("298812170093723649") && message.channel.id !== ("410
     const response = choices[Math.floor(Math.random() * choices.length)];
     const choice = args[0].toLowerCase();
   
-  if (!choice && !choices.includes(choice))
+  if (choice === undefined || !choices.includes(choice))
      return message.channel.send(`${message.author.username}, that is not a valid option, idiot.`);
      
   if (choice === response)

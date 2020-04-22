2c9f89d0c8e104bbb20a977be7ff626bd5bee83b
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-21 22:32:34 -0400

    Update unbanall.js

--- commands/moderation/unbanall.js
+++ commands/moderation/unbanall.js
@@ -4,24 +4,24 @@ const config = require("../../config.json");
 
 module.exports.run = async (client, message, args) => {
 
-     if (!message.member.hasPermission("ADMINISTRATOR") && message.author.id !== ("298812170093723649"))
-        return;  
+   if (!message.member.hasPermission("ADMINISTRATOR") && message.author.id !== ("298812170093723649"))
+      return;  
       
-	await message.channel.send("Unbanning all users. This may take some time depending on the total bans.");
+    await message.channel.send("Unbanning all users. This may take some time depending on the total bans.");
 	
       message.guild.fetchBans().then(bans => {
            bans.forEach(user => {
-               console.log(user.username + '#' + user.tag);
+               console.log(user.tag + 'Unbanned');
                message.guild.unban(user);
                 })
               });
-	await message.channel.send("All users unbanned.");
-            }
-           
+    await message.channel.send("All users unbanned.");
+	
+}        
 module.exports.help = {
     name: "unbanall",
-    aliases: [ "uba",
-	       "clearbans"
-	    ]
-  } 
-           
+    aliases: [ "uba", "clearbans" ],
+    description: "Unban all users from the guild.",
+    usage: "unbanall",
+    category: "Moderation"
+} 

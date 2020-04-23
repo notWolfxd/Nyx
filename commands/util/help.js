46194fb61c5be3f527b556983c88ee1a423d3e19
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-22 20:28:51 -0400

    Update help.js

--- commands/util/help.js
+++ commands/util/help.js
@@ -7,7 +7,7 @@ module.exports.run = async (client, message, args) => {
 
         if (!args.length) {
             data.push('Here\'s a list of all my commands:');
-            data.push(commands.map(command => command.help.name).join(', '));
+            data.push(commands.map(command => command.help.name && command.help.description).join('\n'));
             data.push(`\nYou can send \`-help [command name]\` to get info on a specific command!`);
 
             return message.author.send(data, { split: true })

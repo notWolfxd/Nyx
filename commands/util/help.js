04723868cfc440999636f14a5b1f08b4c3eb7390
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-22 21:41:08 -0400

    Update help.js

--- commands/util/help.js
+++ commands/util/help.js
@@ -1,14 +1,16 @@
 const Discord = require("discord.js");
+const prefixes = require("../../prefixes.json");
 
 module.exports.run = async (client, message, args) => {
 
         const data = [];
         const { commands } = message.client;
+	let prefix = prefixes[message.guild.id].prefixes;
 
         if (!args.length) {
             data.push('Here\'s a list of all my commands:');
-            data.push(commands.map(command => command.help.name).join('\n'));
-            data.push(`\nYou can send \`-help [command name]\` to get info on a specific command!`);
+            data.push(commands.map(command => command.help.name).join(`- ${command.help.description} \n`));
+            data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
 
             return message.author.send(data, { split: true })
                 .then(() => {
@@ -32,8 +34,8 @@ module.exports.run = async (client, message, args) => {
 
 		if (command.help.aliases) data.push(`**Aliases:** ${command.help.aliases.join(', ')}`);
 		if (command.help.description) data.push(`**Description:** ${command.help.description}`);
-		if (command.help.usage) data.push(`**Usage:** ${command.name} ${command.help.usage}`);
-                if (command.help.example) data.push(`**Example:** ${command.help.example}`);
+		if (command.help.usage) data.push(`**Usage:** ${prefix}${command.help.usage}`);
+                if (command.help.example) data.push(`**Example:** ${prefix}${command.help.example}`);
 		if (command.help.category) data.push(`**Category:** ${command.help.category}`);
 
 		message.channel.send(data, { split: true });

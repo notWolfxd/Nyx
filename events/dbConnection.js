f5c9ddcf40f079f4cd6a8bc2577c733c18ba9ab7
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-24 14:19:55 -0400

    Update dbConnection.js

--- events/dbConnection.js
+++ events/dbConnection.js
@@ -2,14 +2,15 @@ const { Client } = require('pg');
 
 exports.run = async (client) => {
 
-const db = new Client({
+const db = await new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: true,
+  max: 20,
+  connectionTimeoutMillis: 0,
+  idleTimeoutMillis: 0
 });
 
-db.connect();
-
-db.run('CREATE TABLE IF NOT EXISTS guildSettings (guildId TEXT, prefix TEXT, casenumber INTEGER, autoroleenabled TEXT, roletogive TEXT, logsenabled TEXT, logschannel TEXT, wlchannel TEXT, wlsystem TEXT, welcomemessage TEXT, slowmodetime INTEGER, modrole TEXT, commandchannel TEXT, blacklisteduser TEXT);')
+await db.query('CREATE TABLE IF NOT EXISTS guildSettings (guildId TEXT, prefix TEXT, casenumber INTEGER, autoroleenabled TEXT, roletogive TEXT, logsenabled TEXT, logschannel TEXT, wlchannel TEXT, wlsystem TEXT, welcomemessage TEXT, slowmodetime INTEGER, modrole TEXT, commandchannel TEXT, blacklisteduser TEXT);')
 .then(() => {
 		console.log("Table created.");
 		})

66e5c46c01a1857acaac53b20d5193eb7dd786f0
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-24 15:00:30 -0400

    Create addGuildDb.js

---
+++ events/addGuildDb.js
@@ -0,0 +1,15 @@
+const db = require("./dbConnection.js");
+
+module.exports = {
+    addGuildtoDB: (guild) => {
+        db.query(`SELECT * FROM guildSettings WHERE guildId ="${guild.id}"`).then(row => {
+            if (!row) {
+            db.query("INSERT INTO guildSettings (guildId, prefix, casenumber, autoroleenabled, roletogive, logsenabled, logschannel, wlchannel, wlsystem, welcomemessage, slowmodetime, modrole, commandchannel, blacklisteduser) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [message.guild.id, "-", 1, "enabled", "none", "enabled", "bot-logs", "welcome", "disabled", "Hello %MENTION%, welcome to %GUILDNAME%.", 3, "Staff", "commands", "none"]);
+             } 
+            }).catch((err) => {
+            console.log(err);
+          
+            db.query("INSERT INTO guildSettings (guildId, prefix, casenumber, autoroleenabled, roletogive, logsenabled, logschannel, wlchannel, wlsystem, welcomemessage, slowmodetime, modrole, commandchannel, blacklisteduser) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [message.guild.id, "-", 1, "enabled", "none", "enabled", "bot-logs", "welcome", "disabled", "Hello %MENTION%, welcome to %GUILDNAME%.", 3, "Staff", "commands", "none"]);
+          })
+    }
+}

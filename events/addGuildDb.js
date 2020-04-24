760061224f5f8fb1060da81ab399dccb19b64130
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-24 17:32:50 -0400

    Update addGuildDb.js

--- events/addGuildDb.js
+++ events/addGuildDb.js
@@ -2,7 +2,9 @@ const db = require("./dbConnection.js");
 
 module.exports = {
     addGuildtoDB: (guild) => {
-        db.query(`SELECT * FROM guildSettings WHERE guildId ="${guild.id}"`).then(row => {
+    db.query('BEGIN', err => {
+   
+      db.query(`SELECT * FROM guildSettings WHERE guildId ="${guild.id}"`).then(row => {
             if (!row) {
             db.query("INSERT INTO guildSettings (guildId, prefix, casenumber, autoroleenabled, roletogive, logsenabled, logschannel, wlchannel, wlsystem, welcomemessage, slowmodetime, modrole, commandchannel, blacklisteduser) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [guild.id, "-", 1, "enabled", "none", "enabled", "bot-logs", "welcome", "disabled", "Hello %MENTION%, welcome to %GUILDNAME%.", 3, "Staff", "commands", "none"]);
              } 
@@ -11,5 +13,14 @@ module.exports = {
           
             db.query("INSERT INTO guildSettings (guildId, prefix, casenumber, autoroleenabled, roletogive, logsenabled, logschannel, wlchannel, wlsystem, welcomemessage, slowmodetime, modrole, commandchannel, blacklisteduser) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [guild.id, "-", 1, "enabled", "none", "enabled", "bot-logs", "welcome", "disabled", "Hello %MENTION%, welcome to %GUILDNAME%.", 3, "Staff", "commands", "none"]);
           })
+        db.query('COMMIT', err => {
+          if (err) {
+            console.error('Error committing transaction', err.stack)
+          }
+          
+        })
+      })
+    
+        
     }
 }

4d41a2d5d8bce374db9f4e270fbeb52037f9825d
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-24 16:33:29 -0400

    Update dbConnection.js

--- events/dbConnection.js
+++ events/dbConnection.js
@@ -4,8 +4,8 @@ const db = new Pool({
   connectionString: process.env.DATABASE_URL,
  // ssl: true,
   max: 20,
-  connectionTimeoutMillis: 10000000000000000000,
-  idleTimeoutMillis: 100000000000000000000000
+  connectionTimeoutMillis: -1,
+  idleTimeoutMillis: -1
 });
 
 db.query('CREATE TABLE IF NOT EXISTS guildSettings (guildId TEXT, prefix TEXT, casenumber INTEGER, autoroleenabled TEXT, roletogive TEXT, logsenabled TEXT, logschannel TEXT, wlchannel TEXT, wlsystem TEXT, welcomemessage TEXT, slowmodetime INTEGER, modrole TEXT, commandchannel TEXT, blacklisteduser TEXT)', (err, res) => {

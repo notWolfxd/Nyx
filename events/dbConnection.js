39400784595c99e5c0c9faf208a7c34950a1cb40
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-24 03:08:09 -0400

    Update dbConnection.js

--- events/dbConnection.js
+++ events/dbConnection.js
@@ -7,6 +7,12 @@ const db = new Client({
 
 db.connect();
 
-db.run('CREATE TABLE IF NOT EXISTS guildSettings (guildId TEXT, prefix TEXT, casenumber INTEGER, autoroleenabled TEXT, roletogive TEXT, logsenabled TEXT, logschannel TEXT, wlchannel TEXT, wlsystem TEXT, welcomemessage TEXT, slowmodetime INTEGER, modrole TEXT, commandchannel TEXT, blacklisteduser TEXT);')
+db.query('CREATE TABLE IF NOT EXISTS guildSettings (guildId TEXT, prefix TEXT, casenumber INTEGER, autoroleenabled TEXT, roletogive TEXT, logsenabled TEXT, logschannel TEXT, wlchannel TEXT, wlsystem TEXT, welcomemessage TEXT, slowmodetime INTEGER, modrole TEXT, commandchannel TEXT, blacklisteduser TEXT);')
+.then(() => {
+		console.log("Table created.");
+		})
+	.catch((err) => {
+		console.log(err);
+		});
 
 module.exports = db;

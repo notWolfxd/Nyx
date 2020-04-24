d0a58c07f2e94847406b87e72e944425613a70fc
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-24 12:28:20 -0400

    Update prefix.js

--- commands/util/prefix.js
+++ commands/util/prefix.js
@@ -6,18 +6,27 @@ const fs = require("fs");
 
 module.exports.run = async (client, message, args) => {
 
-    let prefixes = require("../../prefixes.json");
+  //  let prefixes = require("../../prefixes.json");
 
     if (!message.member.hasPermission("MANAGE_GUILD")) return;
-    if (!args[0]) return message.channel.send(`Usage: ${prefixes[message.guild.id].prefixes}prefix <New Prefix>`);
+db.query(`SELECT prefix FROM guildSettings WHERE guildId ="${message.guild.id}"`).then(row => {
+  if (!row) {
+    db.query("INSERT INTO guildSettings (guildId, prefix) VALUES (?, ?)", [message.guild.id, args[0]]);
+    } else {
+      db.query(`UPDATE guildSettings SET prefix = "${args[0]}" WHERE guildId = ${message.guild.id}`);
+    }
+}).catch((err) => {
+    console.log(err);
+});
+ //   if (!args[0]) return message.channel.send(`Usage: ${prefixes[message.guild.id].prefixes}prefix <New Prefix>`);
 
-    prefixes[message.guild.id] = {
-        prefixes: args[0]
-    };
+ //   prefixes[message.guild.id] = {
+ //       prefixes: args[0]
+ //   };
 
-    fs.writeFile("./prefixes.json", JSON.stringify(prefixes), (err) => {
-        if (err) client.logger.error(err)
-    });
+ //   fs.writeFile("./prefixes.json", JSON.stringify(prefixes), (err) => {
+ //       if (err) client.logger.error(err)
+ //   });
 
     message.channel.send(`Your prefix was set to ${args[0]}`);
 };

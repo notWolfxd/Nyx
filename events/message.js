3c628aba431f1ccd47feedb6f336023f4eba1a3a
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-24 12:50:08 -0400

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,20 +1,32 @@
 const fs = require("fs");
-const config = require("../config.json")
+const config = require("../config.json");
+const db = require("./dbConnection.js");
 
 exports.run = (client, message) => {
+    
+    db.query(`SELECT * FROM guildSettings WHERE guildId ="${message.guild.id}"`).then(row => {
+        if (!row) {
+            db.query("INSERT INTO guildSettings (guildId, prefix, casenumber, autoroleenabled, roletogive, logsenabled, logschannel, wlchannel, wlsystem, welcomemessage, slowmodetime, modrole, commandchannel, blacklisteduser) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [message.guild.id, "-", 1, "enabled", "none", "enabled", "bot-logs", "welcome", "disabled", "Hello %MENTION%, welcome to %GUILDNAME%.", 3, "Staff", "commands", "none");
+          } 
+        })
     
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
     
-    let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+  //  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
 
-    if (!prefixes[message.guild.id]) {
-        prefixes[message.guild.id] = {
-          prefixes: config.prefix
-        };
-      }
+  //  if (!prefixes[message.guild.id]) {
+  //      prefixes[message.guild.id] = {
+  //        prefixes: config.prefix
+  //      };
+  //     }
 
-    let prefix = prefixes[message.guild.id].prefixes;
+  //  let prefix = prefixes[message.guild.id].prefixes;
+       db.query(`SELECT * FROM guildSettings WHERE guildId ="${message.guild.id}"`).then(row => {
+        if (!row) return;
+
+              const prefix = row.prefix
+                if (row.prefix === undefined) return prefix = "-"
    
     
 let args = message.content.slice(prefix.length).trim().split(/ +/g);

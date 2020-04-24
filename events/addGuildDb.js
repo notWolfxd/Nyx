5f73ea5bde4726a353797b155acf0d3b9ebce46b
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-24 17:03:33 -0400

    Update addGuildDb.js

--- events/addGuildDb.js
+++ events/addGuildDb.js
@@ -1,8 +1,8 @@
 const db = require("./dbConnection.js");
 
 module.exports = {
-    addGuildtoDB: (guildId) => {
-        db.query(`SELECT * FROM guildSettings WHERE guildId ="${guildId}"`).then(row => {
+    addGuildtoDB: (guild) => {
+        db.query(`SELECT * FROM guildSettings WHERE guildId ="${guild.id}"`).then(row => {
             if (!row) {
             db.query("INSERT INTO guildSettings (guildId, prefix, casenumber, autoroleenabled, roletogive, logsenabled, logschannel, wlchannel, wlsystem, welcomemessage, slowmodetime, modrole, commandchannel, blacklisteduser) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [guild.id, "-", 1, "enabled", "none", "enabled", "bot-logs", "welcome", "disabled", "Hello %MENTION%, welcome to %GUILDNAME%.", 3, "Staff", "commands", "none"]);
              } 

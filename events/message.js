e8dab3985c9c7027dc69778f4ef1a85d39414797
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-24 17:35:15 -0400

    Update message.js

--- events/message.js
+++ events/message.js
@@ -7,11 +7,11 @@ exports.run = (client, message) => {
     let db;
     db = dbConnect();
     
-    db.query(`SELECT * FROM guildSettings WHERE guildId ="${message.guild.id}"`).then(row => {
-        if (!row) {
-            db.query("INSERT INTO guildSettings (guildId, prefix, casenumber, autoroleenabled, roletogive, logsenabled, logschannel, wlchannel, wlsystem, welcomemessage, slowmodetime, modrole, commandchannel, blacklisteduser) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [message.guild.id, "-", 1, "enabled", "none", "enabled", "bot-logs", "welcome", "disabled", "Hello %MENTION%, welcome to %GUILDNAME%.", 3, "Staff", "commands", "none"]);
-          } 
-        })
+  //  db.query(`SELECT * FROM guildSettings WHERE guildId ="${message.guild.id}"`).then(row => {
+  //      if (!row) {
+  //          db.query("INSERT INTO guildSettings (guildId, prefix, casenumber, autoroleenabled, roletogive, logsenabled, logschannel, wlchannel, wlsystem, welcomemessage, slowmodetime, modrole, commandchannel, blacklisteduser) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [message.guild.id, "-", 1, "enabled", "none", "enabled", "bot-logs", "welcome", "disabled", "Hello %MENTION%, welcome to %GUILDNAME%.", 3, "Staff", "commands", "none"]);
+  //        } 
+  //      })
     
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;

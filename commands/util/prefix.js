4a01d67a7d0bcc1940d4b375a8f32654da14214b
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-24 14:46:40 -0400

    Update prefix.js

--- commands/util/prefix.js
+++ commands/util/prefix.js
@@ -10,11 +10,11 @@ module.exports.run = async (client, message, args) => {
   //  let prefixes = require("../../prefixes.json");
 
     if (!message.member.hasPermission("MANAGE_GUILD")) return;
-db.run(`SELECT prefix FROM guildSettings WHERE guildId ="${message.guild.id}"`).then(row => {
+db.query(`SELECT prefix FROM guildSettings WHERE guildId ="${message.guild.id}"`).then(row => {
   if (!row) {
-    db.run("INSERT INTO guildSettings (guildId, prefix) VALUES (?, ?)", [message.guild.id, args[0]]);
+    db.query("INSERT INTO guildSettings (guildId, prefix) VALUES (?, ?)", [message.guild.id, args[0]]);
     } else {
-      db.run(`UPDATE guildSettings SET prefix = "${args[0]}" WHERE guildId = ${message.guild.id}`);
+      db.query(`UPDATE guildSettings SET prefix = "${args[0]}" WHERE guildId = ${message.guild.id}`);
     }
 }).catch((err) => {
     console.log(err);

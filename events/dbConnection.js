8729a9d0e38b8e0e661a9c51b6b57d956a16e06b
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-24 17:21:34 -0400

    Update dbConnection.js

--- events/dbConnection.js
+++ events/dbConnection.js
@@ -12,14 +12,20 @@ db.connect((err, client, release) => {
   if (err) {
     return console.error('Error acquiring client', err.stack)
   }
-db.query('CREATE TABLE IF NOT EXISTS guildSettings (guildId TEXT, prefix TEXT, casenumber INTEGER, autoroleenabled TEXT, roletogive TEXT, logsenabled TEXT, logschannel TEXT, wlchannel TEXT, wlsystem TEXT, welcomemessage TEXT, slowmodetime INTEGER, modrole TEXT, commandchannel TEXT, blacklisteduser TEXT)', (err, res) => {
-
-if (err) {
-      return console.error('Error executing query', err.stack)
-    }
-    console.log(res.rows)
-})
-})
+	
+db.query('BEGIN', err => {
+    if (shouldAbort(err)) return;
+      db.query('CREATE TABLE IF NOT EXISTS guildSettings (guildId TEXT, prefix TEXT, casenumber INTEGER, autoroleenabled TEXT, roletogive TEXT, logsenabled TEXT, logschannel TEXT, wlchannel TEXT, wlsystem TEXT, welcomemessage TEXT, slowmodetime INTEGER, modrole TEXT, commandchannel TEXT, blacklisteduser TEXT)', (err, res) => {
+        if (shouldAbort(err)) return;
+        db.query('COMMIT', err => {
+          if (err) {
+            console.error('Error committing transaction', err.stack)
+          }
+          
+        
+      })
+    })
+  })
 	
 module.exports = {
     dbConnect: () => {

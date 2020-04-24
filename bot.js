a579067b8cfb2c808bad1dc59ad46bf6ac6fa101
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-24 00:21:28 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
-const client = new Discord.Client({
-fetchAllMembers: true
-});
+const client = new Discord.Client({fetchAllMembers: true, disableEveryone: true});
 const config = require('./config.json');
+const db = require("./events/dbConnection.js");
+
 require("./handler/commandhandler")(client);
 require("./handler/eventhandler")(client);
 

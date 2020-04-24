f7d4ff03a78e3d3936ef25c889ea8a61930b47cc
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-24 13:15:14 -0400

    Update message.js

--- events/message.js
+++ events/message.js
@@ -4,7 +4,7 @@ const db = require("./dbConnection.js");
 
 exports.run = (client, message) => {
     
-    db.query(`SELECT * FROM guildSettings WHERE guildId ="${message.guild.id}"`).then(row => {
+    db.run(`SELECT * FROM guildSettings WHERE guildId ="${message.guild.id}"`).then(row => {
         if (!row) {
             db.query("INSERT INTO guildSettings (guildId, prefix, casenumber, autoroleenabled, roletogive, logsenabled, logschannel, wlchannel, wlsystem, welcomemessage, slowmodetime, modrole, commandchannel, blacklisteduser) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [message.guild.id, "-", 1, "enabled", "none", "enabled", "bot-logs", "welcome", "disabled", "Hello %MENTION%, welcome to %GUILDNAME%.", 3, "Staff", "commands", "none"]);
           } 

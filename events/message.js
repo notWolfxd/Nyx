c015ffeb866f824048485e662244dd1b4f305de8
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-24 19:30:30 -0400

    Update message.js

--- events/message.js
+++ events/message.js
@@ -13,7 +13,7 @@ exports.run = (client, message) => {
            idk = `INSERT INTO gSettings (guildId, prefix, casenumber, autoroleenabled, roletogive, logsenabled, logschannel, wlchannel, wlsystem, welcomemessage, slowmodetime, modrole, commandchannel, blacklisteduser) VALUES ("${message.guild.id}", "-", 1, "enabled", "none", "enabled", "bot-logs", "welcome", "disabled", "Hello %MENTION%, welcome to %GUILDNAME%.", 3, "Staff", "commands", "none")`
          
             db.query(idk, console.log)} 
-        })
+        }) )
     
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;

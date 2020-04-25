89b37038e81d6242800595d28b48db24987fb5fc
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-24 20:27:56 -0400

    Update addGuildDb.js

--- events/addGuildDb.js
+++ events/addGuildDb.js
@@ -4,15 +4,14 @@ module.exports = {
     addGuildtoDB: (guild) => {
     db.query('BEGIN', err => {
    
-      db.query(`SELECT * FROM guildSettings WHERE guildId ="${guild.id}"`).then(row => {
-            if (!row) {
-            db.query("INSERT INTO guildSettings (guildId, prefix, casenumber, autoroleenabled, roletogive, logsenabled, logschannel, wlchannel, wlsystem, welcomemessage, slowmodetime, modrole, commandchannel, blacklisteduser) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [guild.id, "-", 1, "enabled", "none", "enabled", "bot-logs", "welcome", "disabled", "Hello %MENTION%, welcome to %GUILDNAME%.", 3, "Staff", "commands", "none"]);
-             } 
-            }).catch((err) => {
-            console.log(err);
-          
-            db.query("INSERT INTO guildSettings (guildId, prefix, casenumber, autoroleenabled, roletogive, logsenabled, logschannel, wlchannel, wlsystem, welcomemessage, slowmodetime, modrole, commandchannel, blacklisteduser) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [guild.id, "-", 1, "enabled", "none", "enabled", "bot-logs", "welcome", "disabled", "Hello %MENTION%, welcome to %GUILDNAME%.", 3, "Staff", "commands", "none"]);
-          })
+db.query(`SELECT * FROM gSettings WHERE guildId ="${guild.id}"`, (rows) => {
+        if (rows.length < 1) {
+           let idk;
+           idk = `INSERT INTO gSettings(guildId, prefix, casenumber, autoroleenabled, roletogive, logsenabled, logschannel, wlchannel, wlsystem, welcomemessage, slowmodetime, modrole, commandchannel, blacklisteduser) VALUES ("${message.guild.id}", "-", 1, "enabled", "none", "enabled", "bot-logs", "welcome", "disabled", "Hello %MENTION%, welcome to %GUILDNAME%.", 3, "Staff", "commands", "none")`;
+         
+            db.query(idk, console.log)} 
+        })
+        
         db.query('COMMIT', err => {
           if (err) {
             console.error('Error committing transaction', err.stack)

34c4f07dc2716421f1515ca3df3921843b09345b
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-25 02:08:21 -0400

    Update message.js

--- events/message.js
+++ events/message.js
@@ -31,7 +31,7 @@ exports.run = async (client, message) => {
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
     
-  /* let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+   let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
 
     if (!prefixes[message.guild.id]) {
        prefixes[message.guild.id] = {
@@ -39,12 +39,12 @@ exports.run = async (client, message) => {
       };
       }
 
-   let prefix = prefixes[message.guild.id].prefixes; */
-       db.query(`SELECT * FROM guildSettings WHERE guildId ="${message.guild.id}"`).then(row => {
+   let prefix = prefixes[message.guild.id].prefixes; 
+   /*    db.query(`SELECT * FROM guildSettings WHERE guildId ="${message.guild.id}"`).then(row => {
         if (!row) return;
 
              const prefix = row.prefix
-                if (row.prefix === undefined) return prefix = "-" 
+                if (row.prefix === undefined) return prefix = "-" */
            
     
 let args = message.content.slice(prefix.length).trim().split(/ +/g);
@@ -68,5 +68,5 @@ try {
 
 } catch (e) {
 }
- })
+ //})
 }

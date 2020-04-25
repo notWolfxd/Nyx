05a4682d4e866a3c45d6c03d25233fb422d7cd20
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-24 23:46:02 -0400

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -8,7 +8,7 @@ exports.run = (client, guild) => {
     db = dbConnect();
     
     db.query(`SELECT * FROM gSettings WHERE guildId ="${guild.id}"`, (row) => {
-        if (row.length == 0) {
+        if (!row) {
            let idk;
            idk = `INSERT INTO gSettings (guildId, prefix, casenumber, autoroleenabled, roletogive, logsenabled, logschannel, wlchannel, wlsystem, welcomemessage, slowmodetime, modrole, commandchannel, blacklisteduser) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)", [guild.id, "-", 1, "enabled", "none", "enabled", "bot-logs", "welcome", "disabled", "Hello %MENTION%, welcome to %GUILDNAME%.", 3, "Staff", "commands", "none"]`;
             db.query(idk, console.log)} 

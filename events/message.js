27b9068439f83035d8c64b6d018cd95eda075453
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-25 17:59:26 -0400

    Update message.js

--- events/message.js
+++ events/message.js
@@ -43,7 +43,7 @@ exports.run = async (client, message) => {
    const phVal3 = [message.guild.id]
    
    db.query(selGuildPrefix, phVal3, (er, res) => {
-     console.log(res.rows[0])
+     console.log(res.rows[0].prefix)
      const prefix = res.rows[0];
       
       

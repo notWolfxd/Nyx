4bbb7d732d7b7c8bf277ea642134d00a0b9e5f3c
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-25 14:50:57 -0400

    Update wolf.js

--- commands/vanity/wolf.js
+++ commands/vanity/wolf.js
@@ -14,14 +14,14 @@ module.exports.run = async (client, message, args) => {
     db.query(HYPE, valued, (er,res) => {
         console.log(er, res)
        
-/*      if (res.rows.length === 0) {
+      if (res.rows.length === 0) {
     
          const text = "INSERT INTO guildSettings(guildId, prefix, casenumber, autoroleenabled, roletogive, logsenabled, logschannel, wlchannel, wlsystem, welcomemessage, slowmodetime, modrole, commandchannel, blacklisteduser) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *"
          const values = [message.guild.id, "-", 1, "enabled", "none", "enabled", "bot-logs", "welcome", "disabled", "Hello %MENTION%, welcome to %GUILDNAME%.", 3, "Staff", "commands", "none"];
-   */
-    const fix = `DELETE FROM guildSettings *`
+   
+  //  const fix = `DELETE FROM guildSettings *`
     
-   db.query(fix, (err, res) => {
+   db.query(text, values, (err, res) => {
   if (err) {
     console.log(err.stack)
   } else {
@@ -29,7 +29,7 @@ module.exports.run = async (client, message, args) => {
     
   }
 })
-         //   }
+            }
   })
   
 

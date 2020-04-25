1162e9a72f0e6969d3cdadcd9c91ecaafea9ee6f
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-24 23:43:06 -0400

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -7,19 +7,10 @@ exports.run = (client, guild) => {
     let db;
     db = dbConnect();
     
-    db.query(`SELECT * FROM gSettings WHERE guildId ="${guild.id}"`).then(row => {
-        if (!row) {
+    db.query(`SELECT * FROM gSettings WHERE guildId ="${guild.id}"`, (row) => {
+        if (row.length == 0) {
            let idk;
            idk = `INSERT INTO gSettings (guildId, prefix, casenumber, autoroleenabled, roletogive, logsenabled, logschannel, wlchannel, wlsystem, welcomemessage, slowmodetime, modrole, commandchannel, blacklisteduser) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)", [guild.id, "-", 1, "enabled", "none", "enabled", "bot-logs", "welcome", "disabled", "Hello %MENTION%, welcome to %GUILDNAME%.", 3, "Staff", "commands", "none"]`;
             db.query(idk, console.log)} 
-        }).catch(() => { 
-     console.error;
-     db.query("CREATE TABLE IF NOT EXISTS gSettings (guildId TEXT, prefix TEXT, casenumber INTEGER, autoroleenabled TEXT, roletogive TEXT, logsenabled TEXT, logschannel TEXT, wlchannel TEXT, wlsystem TEXT, welcomemessage TEXT, slowmodetime INTEGER, modrole TEXT, commandchannel TEXT, blacklisteduser TEXT").then(() => {
-     let idk;
-           idk = `INSERT INTO gSettings(guildId, prefix, casenumber, autoroleenabled, roletogive, logsenabled, logschannel, wlchannel, wlsystem, welcomemessage, slowmodetime, modrole, commandchannel, blacklisteduser) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)", [guild.id, "-", 1, "enabled", "none", "enabled", "bot-logs", "welcome", "disabled", "Hello %MENTION%, welcome to %GUILDNAME%.", 3, "Staff", "commands", "none"]`;
-            db.query(idk, console.log(err))
-     })
-  
-  })
-
+        })
 }

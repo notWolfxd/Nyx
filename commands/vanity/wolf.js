564d527e8c454ec14b302a8290ea3368963bdebc
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-25 02:42:54 -0400

    Update wolf.js

--- commands/vanity/wolf.js
+++ commands/vanity/wolf.js
@@ -15,10 +15,7 @@ module.exports.run = async (client, message, args) => {
     const text = "INSERT INTO guildSettings(guildId, prefix, casenumber, autoroleenabled, roletogive, logsenabled, logschannel, wlchannel, wlsystem, welcomemessage, slowmodetime, modrole, commandchannel, blacklisteduser) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *"
     const values = [message.guild.id, "-", 1, "enabled", "none", "enabled", "bot-logs", "welcome", "disabled", "Hello %MENTION%, welcome to %GUILDNAME%.", 3, "Staff", "commands", "none"];
    */ 
-    const fix = `DELETE FROM guildSettings WHERE guildSettings.id NOT IN 
-(SELECT id FROM guildSettings (
-    SELECT DISTINCT ON (guildId, prefix) *
-  FROM guildSettings));`
+    const fix = `DELETE FROM guildSettings *`
     
    db.query(fix, (err, res) => {
   if (err) {

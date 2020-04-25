c40cc64d8445b57e88cd7ece0934bc6de952be7d
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-25 01:04:24 -0400

    Update wolf.js

--- commands/vanity/wolf.js
+++ commands/vanity/wolf.js
@@ -1,8 +1,24 @@
 const Discord = require("discord.js");
+const { dbConnect } = require("../../events/dbConnection.js");
 
 module.exports.run = async (client, message, args) => {
+    
+    let db;
+    db = dbConnect();
+    
+    const text = "INSERT INTO gSettings(guildId, prefix, casenumber, autoroleenabled, roletogive, logsenabled, logschannel, wlchannel, wlsystem, welcomemessage, slowmodetime, modrole, commandchannel, blacklisteduser) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *"
+    const values = [message.guild.id, "-", 1, "enabled", "none", "enabled", "bot-logs", "welcome", "disabled", "Hello %MENTION%, welcome to %GUILDNAME%.", 3, "Staff", "commands", "none"]`;
+    
+   db.query(text, values, (err, res) => {
+  if (err) {
+    console.log(err.stack)
+  } else {
+    console.log(res.rows[0])
+   
+  }
+})
 
-    if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("<:error:526841077248819250> You are lacking the permission `MANAGE_CHANNELS`!");
+/*    if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("<:error:526841077248819250> You are lacking the permission `MANAGE_CHANNELS`!");
 
     let helper = await message.guild.roles.find(r => r.name.toLowerCase() == "helper").members.map(member => member.id)
     let mod = await message.guild.roles.find(r => r.name.toLowerCase() == "mod").members.map(member => member.id)
@@ -20,7 +36,7 @@ module.exports.run = async (client, message, args) => {
         await console.log(user.displayName)
     }
 
-    console.log("done!")
+    console.log("done!") */
     
 
 }

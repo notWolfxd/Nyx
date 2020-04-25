fe3f1ee6d314b0b77c4613302795f9d41a4eb552
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-25 13:12:57 -0400

    Update wolf.js

--- commands/vanity/wolf.js
+++ commands/vanity/wolf.js
@@ -7,17 +7,17 @@ module.exports.run = async (client, message, args) => {
     
     let db;
     db = dbConnect();
- /*   const HYPE = `SELECT * FROM guildSettings WHERE guildId="${message.guild.id}"`
+    const HYPE = `SELECT * FROM guildSettings WHERE guildId="${message.guild.id}"`
     
     await db.query(HYPE, (er, row) => {
         if (!row) {
     
     const text = "INSERT INTO guildSettings(guildId, prefix, casenumber, autoroleenabled, roletogive, logsenabled, logschannel, wlchannel, wlsystem, welcomemessage, slowmodetime, modrole, commandchannel, blacklisteduser) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *"
     const values = [message.guild.id, "-", 1, "enabled", "none", "enabled", "bot-logs", "welcome", "disabled", "Hello %MENTION%, welcome to %GUILDNAME%.", 3, "Staff", "commands", "none"];
-   */ 
+   /*
     const fix = `DELETE FROM guildSettings *`
-    
-   db.query(fix, (err, res) => {
+    */
+   db.query(text, values, (err, res) => {
   if (err) {
     console.log(err.stack)
   } else {
@@ -25,8 +25,8 @@ module.exports.run = async (client, message, args) => {
     
   }
 })
-       //     }
- // })
+            }
+  })
   
 
 }

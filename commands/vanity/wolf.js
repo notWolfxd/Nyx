78a836d466c59346bb375a67b3b1bcad621c47ed
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-25 13:15:47 -0400

    Update wolf.js

--- commands/vanity/wolf.js
+++ commands/vanity/wolf.js
@@ -10,7 +10,7 @@ module.exports.run = async (client, message, args) => {
     const HYPE = `SELECT * FROM guildSettings WHERE guildId="${message.guild.id}"`
     
     await db.query(HYPE, (er, row) => {
-        if (!row) {
+        if (!row.guildId) {
     
     const text = "INSERT INTO guildSettings(guildId, prefix, casenumber, autoroleenabled, roletogive, logsenabled, logschannel, wlchannel, wlsystem, welcomemessage, slowmodetime, modrole, commandchannel, blacklisteduser) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *"
     const values = [message.guild.id, "-", 1, "enabled", "none", "enabled", "bot-logs", "welcome", "disabled", "Hello %MENTION%, welcome to %GUILDNAME%.", 3, "Staff", "commands", "none"];

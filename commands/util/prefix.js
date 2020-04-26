bec829edd3a99cf83678edac7e603d4c9201ed48
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-25 20:33:25 -0400

    Update prefix.js

--- commands/util/prefix.js
+++ commands/util/prefix.js
@@ -1,40 +1,29 @@
 const { RichEmbed } = require("discord.js");
-const { stripIndents } = require("common-tags");
-const {getMember, formatDate} = require("../../functions.js");
-const config = require("../../config.json");
-const fs = require("fs");
 const { dbConnect }= require("../../events/dbConnection.js");
 
 module.exports.run = async (client, message, args) => {
-
-  //  let prefixes = require("../../prefixes.json");
-
+ 
+if (!message.member.hasPermission("MANAGE_GUILD")) return;
+if (!args[0]) return message.channel.send("Please specify a new prefix!");
+  
   let db;
   db = dbConnect();
-    if (!message.member.hasPermission("MANAGE_GUILD")) return;
-db.query(`SELECT prefix FROM guildSettings WHERE guildId ="${message.guild.id}"`).then(row => {
-  if (!row) {
-    db.query("INSERT INTO guildSettings (guildId, prefix) VALUES (?, ?)", [message.guild.id, args[0]]);
-    } else {
-      db.query(`UPDATE guildSettings SET prefix = "${args[0]}" WHERE guildId = ${message.guild.id}`);
-    }
-}).catch((err) => {
-    console.log(err);
-});
- //   if (!args[0]) return message.channel.send(`Usage: ${prefixes[message.guild.id].prefixes}prefix <New Prefix>`);
+    
+ /*   const selGuildPrefix = "SELECT prefix FROM guildSettings WHERE guildId = $1"
+    const phVal3 = [message.guild.id]
+   
+      db.query(selGuildPrefix, phVal3, (er, res) => {
+        const prefix = res.rows[0].prefix;
 
- //   prefixes[message.guild.id] = {
- //       prefixes: args[0]
- //   };
-
- //   fs.writeFile("./prefixes.json", JSON.stringify(prefixes), (err) => {
- //       if (err) client.logger.error(err)
- //   });
-
-    message.channel.send(`Your prefix was set to ${args[0]}`);
+    message.channel.send(`Your prefix was set to ${prefix}`);
+        }) */
 };
 
 module.exports.help = {
     name: "prefix",
-    aliases: ["pfx"]
+    aliases: ["pfx"],
+    description: "Change the bot prefix for this guild.",
+    usage: "prefix <NewPrefix>",
+    example: "prefix >",
+    category: "Util"
 }

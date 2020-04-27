0a05de28b2438d57dd7c583015b6fa1715ae39d5
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-04-26 21:51:14 -0400

    Update recruit.js

--- commands/vanity/recruit.js
+++ commands/vanity/recruit.js
@@ -1,59 +1,51 @@
-const Discord = require("discord.js");
+const { RichEmbed } = require("discord.js");
+const { stripIndents } = require("common-tags");
 const config = require("../../config.json");
 const cooldown = new Set();
 
 module.exports.run = async (client, message, args) => {
 
    if (message.channel.id !== ("410526913879080960")) 
-        return;
-
+      return;
    if (cooldown.has(message.author.id)) 
       return message.reply("you must wait 5 hours between recruiting!");
     
-      else {
+   else {
 
-    //Build recruit help Embed
-    var recruitHelpEmbed = new Discord.RichEmbed()
-
+   const recruitHelp = new RichEmbed()
     .setTitle("Command: Recruit")
     .setColor("#f74e00")
     .setThumbnail(message.author.avatarURL)
-    .addField("Description: ", "Recruit people to join your team for either: Factions, Skyblock, or Prison.")
-    .addField("Permissions:", "You must have one of the four leader roles (faction-leader, island-leader, or gang-leader).")
-    .addField("Syntax Formatter: ", "Use https://notwolfxd.github.io/leaders/ to format the recruitment post if you're having trouble.")
-    .addField("Friendly PSA:", "If you are caught using this to advertise other servers, then you will be banned.")
-    .setFooter(`${config.version} | Made By: Wolf#9001`, client.user.avatarURL)
+    .addField("Recruit people to join your team.", stripIndents`\n**Permissions:** Gamemode specific leader role.
+    **Syntax Formatter:** Use https://notwolfxd.github.io/leaders to format your post
+    **Friendly PSA:** If you are caught using this to advertise other servers, you will be banned.`)
+    .setFooter(`${config.version}`, client.user.avatarURL)
 
-    //Define the arguments for recruit.
-    let recruitDefine = args.join(" ").split(" | ");
-    let gametype = recruitDefine[0];
-       if (!recruitDefine[0]) 
-        return message.channel.sendEmbed(recruitHelpEmbed);
+     let recruitDefine = args.join(" ").split(" | ");
+     let gameType = recruitDefine[0];
+       
+   if (!recruitDefine[0]) 
+      return message.channel.send(recruitHelp);
 
-    let teamname = recruitDefine[1];
+     let teamName = recruitDefine[1];
+     let about = recruitDefine[2];
+         about = about.replace(/(-)\s/gi, '\n$&');
+     let req = recruitDefine[3];
+         req = req.replace(/(-)\s/gi, '\n$&');  
+     let link = recruitDefine[4];
 
-    let about = recruitDefine[2];
-    about = about.replace(/(-)\s/gi, '\n$&');
+       let facsaliases = [ "facs", "factions", "faction", "demonic", "heroic", "runic" ];
+       let sbaliases = [ "sb", "skyblock", "island", "unity", "hogwarts", "space" ];
+       let prisonaliases = [ "prison", "prisons", "cell", "gang", "genesis", "dawn" ];
 
-    let req = recruitDefine[3];
-    req = req.replace(/(-)\s/gi, '\n$&');  
-
-    let link = recruitDefine[4];
-
-    let facsaliases = [ "facs", "factions", "faction", "demonic", "heroic", "runic" ];
-    let sbaliases = [ "sb", "skyblock", "island", "unity", "fantasy", "space" ];
-    let prisonaliases = [ "prison", "prisons", "cell", "gang" ];
-
-    //Build the Recruitment message Embed.
-    var recruitEmbed = new Discord.RichEmbed()
-
+   const recruitEmbed = new RichEmbed()
     .setTitle("Team Recruitment!")
     .setColor("#ffa0bb")
     .setThumbnail(message.author.avatarURL)
     .addField("Recruitment Format:", "https://notwolfxd.github.io/leaders")
     .addField("Recruiter:", `${message.author.tag} | ${message.author.id}`)
-    .addField("Recruiting On:", `${gametype}`)
-    .addField("Recruiting For:", `${teamname}`)
+    .addField("Recruiting On:", `${gameType}`)
+    .addField("Recruiting For:", `${teamName}`)
     .addField("About Them:", `${about} `)
     .addField("Their Requirements:", `${req}`)
     .addField("Team Discord:", `${link}`)
@@ -62,33 +54,33 @@ module.exports.run = async (client, message, args) => {
    if (message.member.roles.has("471779470525595668") && facsaliases.includes(recruitDefine[0].toLowerCase())) {
       await client.channels.get("426903359392448522").send(recruitEmbed);
       await message.channel.send("Your recruitment information has been sent to the respective channel. You must wait **__five__** hours to post it again!");
-      await    cooldown.add(message.author.id);
-                  setTimeout(() => {
-               cooldown.delete(message.author.id);
-               }, 18000000);
+      await cooldown.add(message.author.id);
+            setTimeout(() => {
+            cooldown.delete(message.author.id);
+                             }, 18000000);
    }
    if (message.member.roles.has("487054093655998485") && sbaliases.includes(recruitDefine[0].toLowerCase())) {
       await client.channels.get("487053977821904909").send(recruitEmbed);
       await message.channel.send("Your recruitment information has been sent to the respective channel. You must wait **__five__** hours to post it again!");
-      await    cooldown.add(message.author.id);
-                  setTimeout(() => {
-               cooldown.delete(message.author.id);
-               }, 18000000);
+      await cooldown.add(message.author.id);
+            setTimeout(() => {
+            cooldown.delete(message.author.id);
+                             }, 18000000);
    }
    if (message.member.roles.has("515587892525924352") && prisonaliases.includes(recruitDefine[0].toLowerCase())) {
       await client.channels.get("515639377695866881").send(recruitEmbed);
       await message.channel.send("Your recruitment information has been sent to the respective channel. You must wait **__five__** hours to post it again!");
-      await    cooldown.add(message.author.id);
-                  setTimeout(() => {
-               cooldown.delete(message.author.id);
-               }, 18000000);
+      await cooldown.add(message.author.id);
+            setTimeout(() => {
+            cooldown.delete(message.author.id);
+                             }, 18000000);
    }
-       }
-   }
+  }
+ }
  module.exports.help = {
     name: "recruit",
-    aliases: [               
-               "recruitment",
-               "recruiting",
-             ]
+    aliases: [ "recruitment", "recruiting" ],
+    description: "Recruit players to join your team.",
+    usage: "Refer to [this](https://notwolfxd.github.io/leaders) for info on usage.",
+    category: "Vanity"
 }

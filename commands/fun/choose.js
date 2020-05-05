6bf7da87ba0f8b3cbfae0b856b5e888f2c8861b5
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-05-05 00:57:32 -0400

    Update choose.js

--- commands/fun/choose.js
+++ commands/fun/choose.js
@@ -5,7 +5,8 @@ module.exports.run = async (client, message, args) => {
 if (message.author.id !== ("298812170093723649") && message.channel.id !== ("410526913879080960") && message.guild.id !== ("460208972306186252") && message.channel.id !== ("517488355693559818") && message.channel.id !== ("324056323794796544")) return;
 
     let choice = args.join(" ").split(" | ");
-    let response = choice[Math.floor(Math.random() * choice.length)];
+    let choices = []
+    let response = choices[Math.floor(Math.random() * choices.length)];
     
   if (!choice[1]) 
      return message.channel.send(`${message.author.username}, you didn't give much to choose from?`);

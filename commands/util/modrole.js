636b452eaaebc9eea4cce3a4b388e66b81bbde8a
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-05-09 18:53:15 -0400

    Create modrole.js

---
+++ commands/util/modrole.js
@@ -0,0 +1,43 @@
+const { RichEmbed } = require("discord.js");
+const { dbConnect } = require("../../events/dbConnection.js");
+const { getRole } = require("../../functions.js");
+
+module.exports.run = async (client, message, args) => {
+ 
+if (!message.member.hasPermission("MANAGE_GUILD")) return;
+if (!args[0]) return message.channel.send("Please specify a role!");
+
+const role = getRole(message, args.join(" "));
+  
+  let db;
+  db = dbConnect();
+    
+    const selModRole = "SELECT modrole FROM guildSettings WHERE guildId = $1"
+    const phVal3 = [message.guild.id]
+   
+      db.query(selModRole, phVal3, (er, res) => {
+       
+        const updModRole = `UPDATE guildSettings SET modrole = $1 WHERE guildId = $2`
+        const phVals = [role, message.guild.id]
+       
+         db.query(updModRole, phVals, (err, res) => {
+          
+           if (err) { 
+              console.log(err.stack);
+               } else {
+              console.log(`Mod role changed to ${role} in ${message.guild.id}`);
+               }
+           })
+
+    message.channel.send(`Your mod role was set to \`${role}\`!`);
+        }) 
+};
+
+module.exports.help = {
+    name: "modrole",
+    aliases: ["moderatorrole"],
+    description: "Change the moderator role for commands in this guild.",
+    usage: "modrole <Role>",
+    example: "modrole Staff",
+    category: "Util"
+}

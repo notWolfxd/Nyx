615ab7bcea68d78b89838e38b4a78b8e6b3d03c6
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-05-10 01:29:37 -0400

    Update slap.js

--- commands/anime/slap.js
+++ commands/anime/slap.js
@@ -3,9 +3,7 @@ const snekfetch = require("snekfetch");
 const { getMember } = require("../../functions.js");
 
 module.exports.run = async (client, message, args) => {
-
-    if (message.author.id !== ("298812170093723649") && message.author.id !== ("216416135929790464") && message.channel.id !== ("410526913879080960") && message.channel.id !== ("324056323794796544")) return;
-      
+ 
     if (!args[0]) return message.channel.send("You missed your target I assume.");
     
       const user = getMember(message, args[0]);

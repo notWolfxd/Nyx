fa3339029ab77e8d26530ca5a47cabe3e5ede075
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-05-09 21:43:36 -0400

    Update meow.js

--- commands/fun/meow.js
+++ commands/fun/meow.js
@@ -3,8 +3,7 @@ const fetch = require('node-fetch');
 const config = require("../../config.json");
 
 module.exports.run = async (client, message, args) => {
-    if (message.author.id !== ("298812170093723649") && message.channel.id !== ("410526913879080960") && message.guild.id !== ("460208972306186252") && message.channel.id !== ("517488355693559818") && message.channel.id !== ("324056323794796544")) return;
-   
+  
     let data = await fetch(`https://nekos.life/api/v2/img/meow`);
     
     let response = JSON.parse(await data.text());

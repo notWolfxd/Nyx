d70e8ac0c901d4415faf3f9c5ba2227c1a17ca21
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-05-10 01:26:39 -0400

    Update hug.js

--- commands/anime/hug.js
+++ commands/anime/hug.js
@@ -4,8 +4,6 @@ const { getMember } = require("../../functions.js");
 
 module.exports.run = async (client, message, args) => {
 
-    if (message.author.id !== ("298812170093723649") && message.author.id !== ("216416135929790464") && message.channel.id !== ("410526913879080960") && message.channel.id !== ("324056323794796544")) return;
-      
     if (!args[0]) return message.channel.send("You can't hug the air (or I wouldn't be so lonely all the time).");
     
       const user = getMember(message, args[0]);

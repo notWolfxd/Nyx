473d5e244a92bd320205150617649d6bc3f78da8
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-05-10 01:27:10 -0400

    Update kiss.js

--- commands/anime/kiss.js
+++ commands/anime/kiss.js
@@ -3,9 +3,7 @@ const snekfetch = require("snekfetch");
 const { getMember } = require("../../functions.js");
 
 module.exports.run = async (client, message, args) => {
-    
-    if (message.author.id !== ("298812170093723649") && message.author.id !== ("216416135929790464") && message.channel.id !== ("410526913879080960") && message.channel.id !== ("324056323794796544")) return;
-      
+     
     if (!args[0]) return message.channel.send("You can't kiss the air (well you can but you're gay if you do).");
     
       const user = getMember(message, args[0]);

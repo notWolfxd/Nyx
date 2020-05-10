2efcdb76c2abcc9e194c4d6b5b4fc9add0af2ef5
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-05-09 20:57:50 -0400

    Update message.js

--- events/message.js
+++ events/message.js
@@ -26,48 +26,37 @@ exports.run = async (client, message) => {
          }
       })
     
-    if(message.author.bot) return;
-    if(message.channel.type === "dm") return;
-    
-  /* let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
-
-    if (!prefixes[message.guild.id]) {
-       prefixes[message.guild.id] = {
-         prefixes: config.prefix
-      };
-      }
-
-   let prefix = prefixes[message.guild.id].prefixes; */
+      if (message.author.bot) return;
+      if (message.channel.type === "dm") return;
    
-   const selGuildPrefix = "SELECT prefix FROM guildSettings WHERE guildId = $1"
+   const selGuildData = "SELECT prefix, modrole, commandchannel FROM guildSettings WHERE guildId = $1"
    const phVal3 = [message.guild.id]
    
-  await db.query(selGuildPrefix, phVal3, (er, res) => {
-  //   console.log(res.rows[0].prefix)
+  await db.query(selGuildData, phVal3, (er, res) => {
      const prefix = res.rows[0].prefix;
-      
+     const modrole = res.row[0].modrole;
+     const commandchannel = res.rows[0].commandchannel;     
 
+    let args = message.content.slice(prefix.length).trim().split(/ +/g);
+    let cmd = args.shift().toLowerCase();
+    let command;
 
-let args = message.content.slice(prefix.length).trim().split(/ +/g);
-let cmd = args.shift().toLowerCase();
-let command;
-
-if (client.commands.has(cmd)) {
-    command = client.commands.get(cmd);
-} else if (client.aliases.has(cmd)) {
-    command = client.commands.get(client.aliases.get(cmd));
-}
-
-    if (!message.content.startsWith(prefix)) return;
-
-    if (command) {
-    
-        if (message.author.id == config.owner) return message.channel.send(`${message.author.username} Sorry the command has been Disabled!!`);
+      if (client.commands.has(cmd)) {
+         command = client.commands.get(cmd);
+    } else if (client.aliases.has(cmd)) {
+         command = client.commands.get(client.aliases.get(cmd));
     }
-try {
-    command.run(client, message, args, prefix, db);
+      if (message.channel.id !== (commandchannel) && !message.member.roles.has(modrole) && !message.member.hasPermission("ADMINISTRATOR")) return;
+      if (!message.content.startsWith(prefix)) return;
 
-} catch (e) {
-}
- })
-}
+      if (command) {
+        if (message.author.id == config.owner) 
+           return message.channel.send(`${message.author.username} Sorry the command has been Disabled!!`);
+                   }
+  try {
+      command.run(client, message, args, prefix, db);
+      } catch (e) {
+      message.channel.send(e);
+      }
+    })
+  }

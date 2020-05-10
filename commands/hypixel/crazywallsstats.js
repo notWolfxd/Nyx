d5b0d76e0827908155eb47ae4c6cd34242590314
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-05-09 21:40:03 -0400

    Update crazywallsstats.js

--- commands/hypixel/crazywallsstats.js
+++ commands/hypixel/crazywallsstats.js
@@ -1,14 +1,11 @@
 const { RichEmbed } = require("discord.js");
 const Hypixel = require('hypixel-api');
 const { stripIndents } = require("common-tags");
-const config = require("../../config.json")
+const config = require("../../config.json");
 
 module.exports.run = async (client, message, args) => {
 
-    
-  if (message.channel.id !== "410526913879080960" && message.channel.id !== "460217052339372042" && message.author.id !== "298812170093723649") return;
-    
-    let member = message.member;
+    let member = message.author;
     let key = process.env.HYPIXEL_TOKEN;
     let player;
 

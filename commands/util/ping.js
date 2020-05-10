5523af5f76c819a14185d13e255ed5eafe4b3e93
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-05-09 21:05:13 -0400

    Public testing

--- commands/util/ping.js
+++ commands/util/ping.js
@@ -1,11 +1,15 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-  if (message.author.id !== ("298812170093723649") && message.channel.id !== ("410526913879080960")) return;
-const msg = await message.channel.send('Pinging...');
-  msg.edit(":ping_pong: Time: " + Math.round(msg.createdTimestamp - message.createdTimestamp) + "ms \n:heart: Heartbeat: " + Math.round(client.ping) + "ms");
+
+  const msg = await message.channel.send('Pinging...');
+        msg.edit(":ping_pong: Time: " + Math.round(msg.createdTimestamp - message.createdTimestamp) + "ms \n:heart: Heartbeat: " + Math.round(client.ping) + "ms");
 }
 
 module.exports.help = {
-  name: "ping"
+  name: "ping",
+  aliases: [ "pingpong", "isdiscordlagging", "latency" ],
+  description: "Check the bot's ping to Discord.",
+  usage: "ping",
+  category: "Util"
 }

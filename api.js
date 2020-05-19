413efd5b01493917dc972e9fb5e429e1e60277d9
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-05-19 14:45:15 -0400

    Update api.js

--- api.js
+++ api.js
@@ -11,7 +11,7 @@ class Client {
        const respName = JSON.parse( await dataName.text() );
        const playerName = respName.name
      
-       const hypixelPlayerData = await fetch(`https://api.hypixel.net/player?key=${this.key}?name=${playerName}`);
+       const hypixelPlayerData = await fetch(`https://api.hypixel.net/player?key=${key}?name=${playerName}`);
        const hypixelPDResp = JSON.parse( await hypixelPlayerData.text() );
        
        if ( hypixelPDResp.success ) {

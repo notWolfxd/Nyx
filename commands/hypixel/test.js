c7894fedd1cdbb9c65ffa661278a27b9c90d1beb
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-05-19 13:00:03 -0400

    Update test.js

--- commands/hypixel/test.js
+++ commands/hypixel/test.js
@@ -1,9 +1,44 @@
 const { RichEmbed } = require("discord.js");
 const Hypixel = require('hypixel-api');
 const { stripIndents } = require("common-tags");
+const nodef = require("node-fetch");
 const config = require("../../config.json");
 
-module.exports.run = async (client, message, args) => {
+module.exports.run = async (client, message, args, p1, p2) => {
+   
+	const targetType = (p2 ? p1 : 'uuid')
+	const identifier = (p2 ? p2 : p1)
+
+	let targetUUID = (targetType === 'uuid' ? identifier : null)
+
+	if (targetType === 'name') {
+		let playerResolution = await nodef('https://api.mojang.com/profiles/minecraft', 'POST').body([identifier]).send()
+
+		if (playerResolution.statusCode === 200) {
+			let body
+			try {
+				body = JSON.parse(playerResolution.body)
+
+				if (!Array.isArray(body)) throw 'Not array'
+			}
+			catch (err) {
+				throw new Error('Invalid response recieved from Mojang UUID resolution endpoint.')
+			}
+
+			if (body.length > 0) {
+				targetUUID = body[0].id
+			}
+			else {
+				throw new Error('Player does not exist.')
+			}
+		}
+		else {
+			throw new Error('Unexpected HTTP status code from Mojang UUID resolution endpoint.')
+		}
+	}
+
+	return message.channel.send(targetUUID)
+
 
     let member = message.author;
     let key = process.env.HYPIXEL_TOKEN;
@@ -32,7 +67,7 @@ module.exports.run = async (client, message, args) => {
     .setFooter(`${config.version} | Requested By: ${message.author.tag}`, client.user.avatarURL)
 
     message.channel.send(embed);
-
+ }
 }
 
 module.exports.help = {

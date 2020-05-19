a2d5d09db5d7584acd99e8f92965f9d9f9430479
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-05-19 14:48:30 -0400

    Update api.js

--- api.js
+++ api.js
@@ -4,14 +4,14 @@ class Client {
 
    constructor(key) {
    
-   const key = process.env.HYPIXEL_TOKEN
+   this.key = process.env.HYPIXEL_TOKEN
     }
      async getPlayer (message, player = '') {
        const dataName = await fetch(`https://api.mojang.com/users/profiles/minecraft/${player}`);
        const respName = JSON.parse( await dataName.text() );
        const playerName = respName.name
      
-       const hypixelPlayerData = await fetch(`https://api.hypixel.net/player?key=${key}?name=${playerName}`);
+       const hypixelPlayerData = await fetch(`https://api.hypixel.net/player?key=${this.key}?name=${playerName}`);
        const hypixelPDResp = JSON.parse( await hypixelPlayerData.text() );
        
        if ( hypixelPDResp.success ) {

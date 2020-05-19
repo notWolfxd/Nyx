3c001295cacfaeaca57c239ecfe89ee11c4e453a
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-05-19 14:16:27 -0400

    Create api.js

---
+++ api.js
@@ -0,0 +1,31 @@
+const fetch = require("node-fetch");
+
+module.exports => {
+
+class Client {
+
+   constructor(key) {
+   
+    this.key = key
+    }
+     async getPlayer (message, player = '') {
+       const dataName = await nodef(`https://api.mojang.com/users/profiles/minecraft/${player}`);
+       const respName = JSON.parse( await dataName.text() );
+       const playerName = respName.name
+     
+       const hypixelPlayerData = await nodef(`https://api.hypixel.net/player?key=${this.key}?name=${playerName}`);
+       const hypixelPDResp = JSON.parse( await hypixelPlayerData.text() );
+       
+       if ( hypixelPDResp.success ) {
+          return hypixelPDResp
+        }
+        else console.log(parsed.cause || response)
+          }
+         
+      }
+
+   }
+
+}
+
+module.sxports = Client

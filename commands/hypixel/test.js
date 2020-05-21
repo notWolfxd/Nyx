d7069d1b93b403d69c436bd17ad42a12035a58b8
notWolfxd <42623315+notWolfxd@users.noreply.github.com>
2020-05-21 14:13:54 -0400

    Update test.js

--- commands/hypixel/test.js
+++ commands/hypixel/test.js
@@ -13,20 +13,19 @@ console.log(re2sp); */
     let member = message.author;
     let key = process.env.HYPIXEL_TOKEN;
     let player;
+    let profile;
 
     const Hypixel2Client = new Hypixel2(key);
     
     try {
-        player = (await Hypixel2Client.getPlayerStatistics(message, args[0], key)).player
+        player = (await Hypixel2Client.getPlayerStatistics(message, args[0], key)).player; 
+	profile = (await player.stats.SkyBlock.profiles.profile_id);
         }
     catch (err) {
         console.log(err)
         message.channel.send('Hmm, that player doesn\'t seem to exist!')
         return;
                 }
-	let profile = `player.stats.SkyBlock.profiles.profile_id`;/*
-	if (profile === undefined);
-	return message.channel.send("No profile found");*/
 	
   const sbProfile = new Hypixel2(profile);
 	
@@ -37,7 +36,7 @@ console.log(re2sp); */
     .setColor(member.displayHexColor === '#000000' ? '#ffffff' : member.displayHexColor)
 
     .addField('**__Heads Information:__**', stripIndents`Each head gives a different amount of EXP, and can only be obtained from corrupted games.
-    > **Angel of Death Tier:** ${(profile.profile_id || 0)}`)
+    > **Angel of Death Tier:** ${(profile.profile.profile_id || 0)}`)
 
     .setFooter(`${config.version} | Requested By: ${message.author.tag}`, client.user.avatarURL)
 
